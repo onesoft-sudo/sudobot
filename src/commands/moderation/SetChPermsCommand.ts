190204a98887bacbabb447d15c7cf4aa21c636ad
Ar Rakin <rakinar2@gmail.com>
2022-05-30 00:07:02 +0600

    Add -setchperms command

---
+++ src/commands/moderation/SetChPermsCommand.ts
@@ -0,0 +1,205 @@
+import { CategoryChannel, CommandInteraction, Message, TextChannel, Permissions, PermissionFlags, PermissionString, GuildChannel, Role } from 'discord.js';
+import BaseCommand from '../../utils/structures/BaseCommand';
+import DiscordClient from '../../client/Client';
+import CommandOptions from '../../types/CommandOptions';
+import InteractionOptions from '../../types/InteractionOptions';
+import MessageEmbed from '../../client/MessageEmbed';
+import getUser from '../../utils/getUser';
+import History from '../../automod/History';
+import { fetchEmoji } from '../../utils/Emoji';
+import getRole, { getRoleRaw } from '../../utils/getRole';
+import { channelMention } from '@discordjs/builders';
+
+export default class BanCommand extends BaseCommand {
+    supportsInteractions: boolean = true;
+
+    constructor() {
+        super('setchperms', 'moderation', []);
+    }
+
+    async run(client: DiscordClient, msg: Message | CommandInteraction, options: CommandOptions | InteractionOptions) {
+        if (!options.isInteraction && typeof options.args[3] === 'undefined') {
+            await msg.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setColor('#f14a60')
+                    .setDescription(`This command requires at least four arguments.`)
+                ]
+            });
+
+            return;
+        }
+
+        if (msg instanceof CommandInteraction)
+            await msg.deferReply();
+        else {
+            msg = await msg.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setColor('GOLD')
+                    .setDescription((await fetchEmoji('loading'))?.toString() + ' Working...')
+                ]
+            });
+        }
+
+        const { FLAGS } = Permissions;
+        
+        let channels: (TextChannel | CategoryChannel)[] = [];
+        let permKey: PermissionString;
+        let permValue: null | boolean = null;
+        let role: Role;
+
+        if (options.isInteraction) {
+            channels.push(<TextChannel | CategoryChannel> options.options.getChannel('channel'));
+
+            if (channels[0].type !== 'GUILD_CATEGORY' && channels[0].type !== 'GUILD_TEXT') {
+                await this.deferReply(msg, {
+                    content: (await fetchEmoji('error'))?.toString() + ' The channel with ID ' + (channels[0] as GuildChannel).id + ' is not a text channel or category.',
+                    embeds: []
+                }, true);
+
+                return;
+            }
+
+            permKey = <PermissionString> options.options.getString('permission');
+            const permValueInput = <string> options.options.getString('value');
+
+            if (permValueInput === 'true')
+                permValue = true;
+            else if (permValueInput === 'false')
+                permValue = false;
+            else
+                permValue = null;
+
+            role = <Role> options.options.getRole('role');
+        }
+        else {
+            const permValueInput = options.args.pop();
+
+            if (permValueInput === 'true')
+                permValue = true;
+            else if (permValueInput === 'false')
+                permValue = false;
+            else if (permValueInput === 'null')
+                permValue = null;
+            else {
+                await this.deferReply(msg, {
+                    content: (await fetchEmoji('error'))?.toString() + ' Invalid permission value given, permission values must be one of these: `null`, `true`, `false`.',
+                    embeds: []
+                }, true);
+
+                return;
+            }
+
+            permKey = <PermissionString> options.args.pop();
+
+            if (FLAGS[permKey] === undefined) {
+                await this.deferReply(msg, {
+                    content: (await fetchEmoji('error'))?.toString() + ' Invalid permission key given.',
+                    embeds: []
+                }, true);
+
+                return;
+            }
+
+            if (options.args[options.args.length - 1] === 'everyone') {
+                role = msg.guild!.roles.everyone;
+            }
+            else {
+                try {
+                    role = <Role> await getRoleRaw(options.args[options.args.length - 1], msg.guild!);
+    
+                    if (!role)
+                        throw new Error();
+                }
+                catch (e) {
+                    console.log(e);
+                    
+                    await this.deferReply(msg, {
+                        content: (await fetchEmoji('error'))?.toString() + ' Invalid role given.',
+                        embeds: []
+                    }, true);
+    
+                    return;
+                }
+            }
+
+            options.args.pop();
+
+            for await (let chID of options.args) {
+                if (/^\d+$/g.test(chID)) {
+                    let channel: CategoryChannel | TextChannel;
+
+                    try {
+                        channel = <typeof channel> (await msg.guild!.channels.fetch(chID))!;
+
+                        if (channel.type !== 'GUILD_CATEGORY' && channel.type !== 'GUILD_TEXT') {
+                            await this.deferReply(msg, {
+                                content: (await fetchEmoji('error'))?.toString() + ' The channel with ID ' + chID + ' is not a text channel or category.',
+                                embeds: []
+                            }, true);
+    
+                            return;
+                        }
+
+                        if (channel.type === 'GUILD_CATEGORY') {
+                            channels = [...channel.children.filter(c => c.type === 'GUILD_TEXT').toJSON() as TextChannel[], ...channels];
+                            continue;
+                        }
+                    }
+                    catch (e) {
+                        console.log(e);
+                        
+                        await this.deferReply(msg, {
+                            content: (await fetchEmoji('error'))?.toString() + ' The channel with ID ' + chID + ' could not be fetched.',
+                            embeds: []
+                        }, true);
+
+                        return;
+                    }
+                    
+                    channels.push(channel);
+                }
+            }
+        }
+
+        if (FLAGS[permKey] === undefined) {
+            await this.deferReply(msg, {
+                content: (await fetchEmoji('error'))?.toString() + ' Invalid permission key given.',
+                embeds: []
+            }, true);
+
+            return;
+        }
+
+        let affected = '';
+
+        for await (const channel of channels) {
+            try {
+                await channel.permissionOverwrites.edit(role, {
+                    [permKey]: permValue
+                });
+
+                affected += `${channelMention(channel.id)} (${channel.id})\n`;
+            }
+            catch (e) {
+                console.log(e);
+                
+                await this.deferReply(msg, {
+                    content: (await fetchEmoji('error'))?.toString() + ' Failed to set permissions for channel ' + channel.id,
+                    embeds: []
+                }, true);
+    
+                return;
+            }
+        }
+
+        await this.deferReply(msg, {
+            embeds: [
+                new MessageEmbed()
+                .setColor('GREEN')
+                .setDescription(`${(await fetchEmoji('check'))?.toString()} Permissions updated!\nThese channels were affected:\n\n${affected}`)
+            ]
+        }, true);
+    }
+}
--- src/utils/getRole.ts
+++ src/utils/getRole.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, GuildMember, Message, Role, User } from "discord.js";
+import { CommandInteraction, Guild, GuildMember, Message, Role, User, MessageMentions } from "discord.js";
 import CommandOptions from "../types/CommandOptions";
 
 export default async function getRole(msgInteraction: Message, options: CommandOptions, index: number = 0): Promise<Role | null | undefined> {
@@ -11,4 +11,15 @@ export default async function getRole(msgInteraction: Message, options: CommandO
     const arg = await options.normalArgs[index];
 
     return await msgInteraction.guild?.roles.fetch(arg);
+}
+
+export async function getRoleRaw(roleString: string, guild: Guild): Promise<Role | null | undefined> {
+    if (roleString === '@everyone')
+        return guild.roles.everyone;
+    else if (MessageMentions.ROLES_PATTERN.test(roleString)) {
+        roleString = roleString.substring(3, roleString.length - 1);
+    }
+    
+    console.log(roleString);        
+    return await guild.roles.fetch(roleString);
 }
