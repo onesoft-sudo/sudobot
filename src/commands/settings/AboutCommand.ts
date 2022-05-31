9c3d59f8c266745efec3c447890a59090e50dc2f
Ar Rakin <rakinar2@gmail.com>
2022-05-31 21:31:39 +0600

    Bump version number

--- src/commands/settings/AboutCommand.ts
+++ src/commands/settings/AboutCommand.ts
@@ -4,13 +4,15 @@ import DiscordClient from '../../client/Client';
 import CommandOptions from '../../types/CommandOptions';
 import InteractionOptions from '../../types/InteractionOptions';
 import MessageEmbed from '../../client/MessageEmbed';
-import Help from '../../utils/help';
 
 export default class AboutCommand extends BaseCommand {
     supportsInteractions: boolean = true;
+    version: string;
 
     constructor() {
         super('about', 'settings', []);
+        const { version } = require('../../../package.json');
+        this.version = version;
     }
 
     async run(client: DiscordClient, message: Message | CommandInteraction, options: CommandOptions | InteractionOptions) {
@@ -19,7 +21,7 @@ export default class AboutCommand extends BaseCommand {
                 new MessageEmbed()
                 .setTitle('SudoBot')
                 .setDescription('A free and open source discord moderation bot, specially created for **The Everything Server**.')
-                .addField('Version', Help.version)
+                .addField('Version', this.version)
                 .addField('Support', 'rakinar2@gmail.com')
                 .setFooter({
                     text: 'Copyright © Ar Rakin 2022, all rights reserved'
