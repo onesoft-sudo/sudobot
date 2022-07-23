7fc213dbcfa126ee7056279f8a1e37f19e0c24d2
Ar Rakin <rakinar2@gmail.com>
2022-07-23 16:14:13 +0600

    fix(utils): reading users/members in the wrong way

--- src/utils/getUser.ts
+++ src/utils/getUser.ts
@@ -2,23 +2,32 @@ import { CommandInteraction, GuildMember, Message, User } from "discord.js";
 import DiscordClient from "../client/Client";
 import CommandOptions from "../types/CommandOptions";
 import InteractionOptions from "../types/InteractionOptions";
+import { parseUser } from './parseInput';
 
 export default async function getUser(client: DiscordClient, msgInteraction: Message, options: CommandOptions, index: number = 0): Promise<User | null | undefined> {
     if (options.normalArgs[index] === undefined) 
         return null;
     
-    console.log('here');
-    
-    if (msgInteraction.mentions.users?.at(index))
-        return await msgInteraction.mentions.users?.at(index);
-
-    console.log(2);
-    
     const arg = await options.normalArgs[index];
+
+	console.log(arg);
 
     if (arg.indexOf('#') !== -1) {
         return await client.users.cache.find(user => user.tag === arg);
     }
 
-    return await client.users.fetch(arg);
+    const parsed = await parseUser(client, arg);
+
+    if (parsed) {
+    	return parsed;
+    }
+
+	try {
+    	const u = await client.users.fetch(arg);
+    	return u;
+    }
+    catch (e) {
+    	console.log(e);
+    	return null;
+    }
 }
