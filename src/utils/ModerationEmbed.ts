e393943ce82c114a449c62e419bdeb12cd627437
Ar Rakin <rakinar2@gmail.com>
2022-07-23 20:29:28 +0600

    feat(utils): add a dedicated class for moderation command embeds

---
+++ src/utils/ModerationEmbed.ts
@@ -0,0 +1,26 @@
+import { MessageEmbed, User, MessageEmbedOptions } from 'discord.js';
+
+export class ModerationEmbed extends MessageEmbed {
+	constructor(protected user: User, protected mod: User, options?: MessageEmbedOptions) {
+		super({
+			author: {
+				name: user.tag,
+				iconURL: user.displayAvatarURL()
+			},
+			...options
+		});
+		
+		this.addField('Executor', [
+			`Tag: ${mod.tag}`,
+			`ID: ${mod.id}`
+		]);
+
+		this.setFooter({
+			text: `${user.id}`
+		});
+
+		this.setTimestamp();
+		
+		this.setColor('#007bff');
+	}
+}
