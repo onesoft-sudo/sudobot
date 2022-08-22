d23c68d2e20c908e7b27ded19b01c4ee51bd84f1
Ar Rakin <rakinar2@gmail.com>
2022-08-22 16:42:15 +0000

    chore: integrate with PM2 process manager (#53)

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+        {
+            script: 'build/index.js',
+            env_production: {
+                NODE_ENV: "production"
+            },
+            env_development: {
+                NODE_ENV: "development"
+            },
+            max_memory_restart: "850M"
+        }
+    ],
+
+};
