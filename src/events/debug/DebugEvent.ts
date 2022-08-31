c8374384ec0b142391740795bde255e740e9d2bb
Ar Rakin <rakinar2@gmail.com>
2022-08-31 09:37:17 +0000

    feat: add debug logger

--- src/events/debug/DebugEvent.ts
+++ src/events/debug/DebugEvent.ts
@@ -1,11 +1,24 @@
+import { format } from "date-fns";
+import { appendFile } from "fs/promises";
+import path from "path";
+import DiscordClient from "../../client/Client";
 import BaseEvent from "../../utils/structures/BaseEvent";
 
 export default class DebugEvent extends BaseEvent {
+    logFile: string = path.resolve(__dirname, "..", "..", "..", "logs", 'debug.log');
+
     constructor() {
         super("debug");
+        appendFile(this.logFile, `\n\n[${format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")}] [STARTUP] The system has started.\n`);
     }
 
-    async run(e: any): Promise <void> {
-       console.log("DEBUG: ", e);
+    async run(client: DiscordClient, e: string): Promise <void> {
+        if (e.includes("Provided token") || e.includes(process.env.TOKEN!) || e.includes("[READY] Session ")) {
+            console.log('DEBUG: [One hidden log]');
+            return;
+        }
+
+        console.log("DEBUG: ", e);
+        await appendFile(this.logFile, `[${format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")}] [LOG] ${e}\n`);
     }
 }
