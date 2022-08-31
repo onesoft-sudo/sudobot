f26342a1544094a04c839268d9b7fc73a15825ec
Ar Rakin <rakinar2@gmail.com>
2022-08-31 11:04:34 +0000

    perf: exec `kill 1` when login error occurs in replit

--- src/events/debug/DebugEvent.ts
+++ src/events/debug/DebugEvent.ts
@@ -1,6 +1,8 @@
+import { exec } from "child_process";
 import { format } from "date-fns";
 import { appendFile } from "fs/promises";
 import path from "path";
+import { exit } from "process";
 import DiscordClient from "../../client/Client";
 import BaseEvent from "../../utils/structures/BaseEvent";
 
@@ -15,6 +17,11 @@ export default class DebugEvent extends BaseEvent {
     async run(client: DiscordClient, e: string): Promise <void> {
         if (e.includes("Provided token") || e.includes(process.env.TOKEN!) || e.includes("[READY] Session ")) {
             console.log('DEBUG: [One hidden log]');
+            return;
+        }
+
+        if (process.env.PLATFORM === 'replit' && e.includes("Hit a 429 while executing a request") && !client.isReady()) {
+            exec("kill 1");
             return;
         }
 
