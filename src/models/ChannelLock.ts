3b917076e8285b4ae84e86043fbf9282cf762f2b
Ar Rakin <rakinar2@gmail.com>
2022-09-05 15:55:09 +0000

    refactor: remove unneeded comments

--- src/models/ChannelLock.ts
+++ src/models/ChannelLock.ts
@@ -2,14 +2,6 @@ import { DataTypes, Model } from 'sequelize';
 import DiscordClient from '../client/Client';
 import { Schema, model, Document, SchemaTypes } from 'mongoose';
 
-// export interface IChannelLock extends Document {
-//     user: string;
-//     reason?: string;
-//     mentions: Array<object>;
-//     guild_id: string;
-//     createdAt: Date;
-// }
-
 const schema = new Schema({
     reason: {
         type: String,
@@ -41,45 +33,5 @@ const schema = new Schema({
         required: true
     }
 });
-
-// class ChannelLock extends Model {}
-
-// ChannelLock.init({
-//     id: {
-//         type: DataTypes.INTEGER,
-//         autoIncrement: true,
-//         primaryKey: true,
-//     },
-//     reason: {
-//         type: DataTypes.TEXT,
-//         allowNull: true
-//     },
-//     user_id: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     guild_id: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     channel_id: {
-//         type: DataTypes.STRING,
-//         unique: true,
-//         allowNull: false
-//     },
-//     role_id: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     previous_perms: {
-//         type: DataTypes.JSON,
-//         allowNull: false
-//     }
-// }, {
-//     sequelize: DiscordClient.client.db.sequelize,
-//     modelName: 'ChannelLock',
-//     updatedAt: false,
-//     tableName: 'channel_lock'
-// });
 
 export default model('ChannelLock', schema);
