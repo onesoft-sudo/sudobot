81aae50933d273d6ecca5f85fb1a158b8b6ec009
Ar Rakin <rakinar2@gmail.com>
2022-09-05 12:23:11 +0000

    feat(api): advanced response handling

--- src/api/controllers/MainController.ts
+++ src/api/controllers/MainController.ts
@@ -2,13 +2,7 @@ import { Request } from "express";
 import Controller from "../Controller";
 
 export default class MainController extends Controller {
-    public index(request: Request) {
-        console.log(request.method);
-        
-        return "Hello world";
-    }
-
-    public system(request: Request) {
-        return "Hello world 2";
+    public async index(request: Request) {
+        return { message: "Server is up." };
     }
 }
---
+++ src/types/KeyValuePair.ts
@@ -0,0 +1,3 @@
+export default interface KeyValuePair<T> {
+    [key: string]: T;
+}
