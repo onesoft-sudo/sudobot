a1e21e5895cc7cfb398d5fca891c090fb7a8ed3f
Ar Rakin <rakinar2@gmail.com>
2022-09-05 10:27:46 +0000

    chore: remove all api files

--- src/api/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { Route } from "../Router";
-
-export default <Route> {
-    path: '/',
-    all(req: Request, res: Response) {
-        res.send('API is up.');
-    }
-};
