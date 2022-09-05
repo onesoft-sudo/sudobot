5a5784a5a596ec4e5d5ec4033a7ab93725fe3b9b
Ar Rakin <rakinar2@gmail.com>
2022-09-05 12:01:41 +0000

    feat: improve controller handling

--- src/api/routes/api.ts
+++ src/api/routes/api.ts
@@ -2,3 +2,4 @@ import MainController from "../controllers/MainController";
 import Router from "../Router";
 
 Router.get("/", [MainController, "index"]);
+Router.get("/system", [MainController, "system"]);
