9bc634067b53634c8c88cc40e3fcf7b8b70af00b
Ar Rakin <rakinar2@gmail.com>
2022-09-06 15:03:54 +0000

    refactor(db): use mongodb

--- src/client/Database.ts
+++ src/client/Database.ts
@@ -6,77 +6,13 @@ import mongoose from "mongoose";
 export default class Database {
     client: DiscordClient;
     dbpath: string;
-    db: DB;
-    sequelize: Sequelize;
 
     constructor(dbpath: string, client: DiscordClient) {
         this.client = client;
         this.dbpath = dbpath;
 
-        this.sequelize = new Sequelize({
-            dialect: 'sqlite',
-            storage: dbpath,
-        });
-
-        this.db = new DB(dbpath, (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-
         mongoose.connect(process.env.MONGO_URI!)
             .then(() => console.log("Connected to MongoDB"))
             .catch(console.error);
     }
-
-    get(sql: string, params: any[] | Function, callback?: Function) {
-        return this.db.get(sql, params, callback);
-    }
-
-    all(sql: string, params: any[] | Function, callback?: Function) {
-        return this.db.all(sql, params, callback);
-    }
-
-    runAsync(sql: string, paramsOrCallback: any[] | Function = []) {
-        return new Promise<void>((resolve, reject) => {
-            this.db.run(sql, paramsOrCallback, err => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                resolve();
-            });
-        });
-    }
-
-    getAsync(sql: string, paramsOrCallback: any[] | Function = []): Promise <any> {
-        return new Promise((resolve, reject) => {
-            this.db.get(sql, paramsOrCallback, (err, data) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                resolve(data);
-            });
-        });
-    }
-
-    allAsync(sql: string, paramsOrCallback: any[] | Function = []): Promise <any> {
-        return new Promise((resolve, reject) => {
-            this.db.all(sql, paramsOrCallback, (err, data) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                resolve(data);
-            });
-        });
-    }
-
-    get s(): Sequelize {
-        return this.sequelize;
-    }
-};
+}
