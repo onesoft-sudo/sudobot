20b8dbcdc970a29d4a8639d164b708d7d7e601ae
Ar Rakin <rakinar2@gmail.com>
2022-09-06 14:43:33 +0000

    refactor(queuejobs): use mongodb

--- src/utils/setTimeout.ts
+++ src/utils/setTimeout.ts
@@ -5,39 +5,43 @@ import Timeout from "../models/Timeout";
 const timeouts = new Map();
 
 export const runTimeouts = async () => {
-    const data = (await Timeout.findAll());
+    const data = (await Timeout.find());
 
     // await console.log(data);
 
     if (data && data.length > 0) {
         console.log('Running timeouts...');
         for await (const row of data) {
-            // console.log(row)
-
             let timeout = await setTimeout(async () => {
                 await console.log('TIMEOUT');
-                await DiscordClient.client.db.runAsync("DELETE FROM timeouts WHERE id = ?", [row.get('id')]);
+                await row.delete();
                 await timeouts.delete(row.get('id'));
                 (await import(row.get('filePath') as string)).default(DiscordClient.client, ...JSON.parse(row.get('params') as string));
             }, new Date(row.get('time') as string).getTime() - Date.now());
 
-            await timeouts.set(row.get('id'), {
-                row,
-                timeout
-            });
+            // await timeouts.set(row.get('id'), {
+            //     row,
+            //     timeout
+            // });
         }
     }
 };
 
 export const setTimeoutv2 = async (file: string, time: number, guild_id: string, cmd: string, ...params: any[]) => {
     await console.log('SETTING');
-    await DiscordClient.client.db.allAsync("INSERT INTO timeouts(created_at, filePath, time, params, guild_id, cmd) VALUES(?, ?, ?, ?, ?, ?)", [new Date().toISOString(), path.resolve(__dirname, '../queues', file), new Date(Date.now() + time).toISOString(), JSON.stringify(params), guild_id, cmd]);
-    const row = await DiscordClient.client.db.getAsync('SELECT * FROM timeouts ORDER BY id DESC LIMIT 0, 1');
+    const row = await Timeout.create({
+        createdAt: new Date(),
+        filePath: path.resolve(__dirname, '../queues', file), 
+        time: new Date(Date.now() + time).toISOString(), 
+        params: JSON.stringify(params), 
+        guild_id, 
+        cmd
+    })
 
     const timeout = await setTimeout(async () => {
         await console.log('TIMEOUT_SET');
-        await DiscordClient.client.db.runAsync("DELETE FROM timeouts WHERE id = ?", [row.id]);
-        await timeouts.delete(row.id);      
+        await timeouts.delete(row.id);   
+        await row.delete();   
         (await import(path.resolve(__dirname, '../queues', file))).default(DiscordClient.client, ...params);
     }, time);
     
@@ -52,8 +56,8 @@ export const setTimeoutv2 = async (file: string, time: number, guild_id: string,
 
 export const clearTimeoutv2 = async ({ row, timeout }: any) => {
     await clearTimeout(timeout);
-    await DiscordClient.client.db.runAsync("DELETE FROM timeouts WHERE id = ?", [row.id]);
     await timeouts.delete(row.id);
+    await row.delete();
 }; 
 
 export const getTimeout = (id: string | number) => timeouts.get(id);
