e0db0110bee2b6db3c3d009b8214f76bb993604c
Ar Rakin <rakinar2@gmail.com>
2022-09-23 12:09:58 +0000

    chore: add license notice in the about command (#76)

--- src/commands/settings/AboutCommand.ts
+++ src/commands/settings/AboutCommand.ts
@@ -7,24 +7,49 @@ import MessageEmbed from '../../client/MessageEmbed';
 
 export default class AboutCommand extends BaseCommand {
     supportsInteractions: boolean = true;
-    version: string;
+    metadata = require('../../../package.json');
 
     constructor() {
         super('about', 'settings', []);
-        const { version } = require('../../../package.json');
-        this.version = version;
     }
 
     async run(client: DiscordClient, message: Message | CommandInteraction, options: CommandOptions | InteractionOptions) {
         await message.reply({
             embeds: [
                 new MessageEmbed()
-                .setTitle('SudoBot')
-                .setDescription('A free and open source discord moderation bot, specially created for **The Everything Server**.')
-                .addField('Version', this.version)
-                .addField('Support', 'rakinar2@gmail.com')
+                .setAuthor({ iconURL: client.user?.displayAvatarURL(), name: "SudoBot" })
+                .setDescription(`
+                    A free and open source Discord moderation bot, specially created for **The Everything Server**.
+
+                    Copyright (C) ${new Date().getFullYear()} OSN Inc.
+                    This bot comes with ABSOLUTELY NO WARRANTY.
+                    This is free software, and you are welcome to redistribute it under certain conditions.
+                    See the [GNU Affero General Public License v3](https://www.gnu.org/licenses/agpl-3.0.en.html) for more detailed information.
+                `)
+                .addFields({
+                    name: 'Version',
+                    value: this.metadata.version,
+                    inline: true
+                }, {
+                    name: 'Source Code',
+                    value: `[GitHub](${this.metadata.repository.url})`,
+                    inline: true,
+                }, {
+                    name: 'Licensed Under',
+                    value: `[GNU Affero General Public License v3](https://www.gnu.org/licenses/agpl-3.0.en.html)`,
+                    inline: true
+                })
+                .addFields({
+                    name: "Author",
+                    value: `[${this.metadata.author.name}](${this.metadata.author.url})`,
+                    inline: true
+                }, {
+                    name: 'Support',
+                    value: this.metadata.author.email,
+                    inline: true
+                })
                 .setFooter({
-                    text: 'Copyright © Ar Rakin 2022, all rights reserved'
+                    text: `Copyright © OSN Inc 2022. All rights reserved.`
                 })
             ]
         });
