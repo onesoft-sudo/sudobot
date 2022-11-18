d51b6c4dcdce2c8c1ef2263a8e56bc8060a7904f
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2022-11-18 21:51:29 +0600

    fix: workflow error

--- src/commands/automation/EmbedSendCommand.ts
+++ src/commands/automation/EmbedSendCommand.ts
@@ -47,7 +47,7 @@ export default class EmbedSendCommand extends BaseCommand {
             iconURL: getString('footer_iconurl'),
         };
 
-        if (getString('color') && (!Util.resolveColor(getString('color') as ColorResolvable) || Util.resolveColor(getString('color') as ColorResolvable) === NaN)) {
+        if (getString('color') && (!Util.resolveColor(getString('color') as ColorResolvable) || isNaN(Util.resolveColor(getString('color') as ColorResolvable)))) {
             await interaction.reply({ content: "Invalid color given.", ephemeral: true });
             return;
         }
