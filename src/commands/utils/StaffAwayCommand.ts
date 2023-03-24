cd7d97a9dae07383148fbf4bcdf1a57005093c7b
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-03-24 12:38:47 +0000

    feat: staff away system

---
+++ src/commands/utils/StaffAwayCommand.ts
@@ -0,0 +1,68 @@
+/**
+* This file is part of SudoBot.
+* 
+* Copyright (C) 2021-2022 OSN Inc.
+*
+* SudoBot is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Affero General Public License as published by 
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* SudoBot is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License 
+* along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { CommandInteraction, Message, Util } from 'discord.js';
+import BaseCommand from '../../utils/structures/BaseCommand';
+import DiscordClient from '../../client/Client';
+import CommandOptions from '../../types/CommandOptions';
+import InteractionOptions from '../../types/InteractionOptions';
+import { formatDuration, intervalToDuration } from 'date-fns';
+import StaffAway from '../../models/StaffAway';
+import { emoji } from '../../utils/Emoji';
+
+export default class AFKCommand extends BaseCommand {
+    supportsInteractions = true;
+
+    constructor() {
+        super('staffaway', 'utils', ['safk', 'saway']);
+    }
+
+    async run(client: DiscordClient, message: Message, options: CommandOptions) {
+        let status: string | undefined = message.content.slice(client.config.props[message.guildId!].prefix.length).trim().slice(options.cmdName.length).trim();
+
+        if (message instanceof Message) {
+            status = status?.trim() === '' ? undefined : status;
+        }
+        
+        if (status && status.replace(/<\w+\:\d+>/gi, '').length > 4000) {
+            message.reply(":x: The reason is too long. Make sure it has less than 4000 characters.").catch(console.error);
+            return;
+        }
+
+        const entry = await StaffAway.findOne({ user: message.author.id });
+
+        if (!entry) {
+            const newEntry = await StaffAway.create({
+                user: message.author.id,
+                createdAt: new Date(),
+                guild_id: message.guildId!,
+                reason: status
+            });
+
+            client.utils.staffAwayList.push(newEntry);
+
+            await message.reply(`${emoji('check')} Thanks for letting us know that you will be away.`);
+        }
+        else {
+            client.utils.findStaffAway(message.guildId!, message.author.id, true);
+            await entry.delete();
+            await message.reply(`${emoji('check')} Welcome back, we removed your away status.`);
+        }
+    }
+}
--- src/services/Utilities.ts
+++ src/services/Utilities.ts
@@ -1,8 +1,53 @@
-import { Message } from "discord.js";
+import { userMention } from "@discordjs/builders";
+import { Collection, Message } from "discord.js";
+import DiscordClient from "../client/Client";
+import StaffAway, { IStaffAway } from "../models/StaffAway";
 import Service from "../utils/structures/Service";
 
 export default class Utilities extends Service {
     typingInterval: NodeJS.Timer | null = null;
     typingTimeOut: NodeJS.Timeout | null = null;
     lastDeletedMessage?: Message;
+    staffAwayList: IStaffAway[] = [];
+
+    constructor(client: DiscordClient) {
+        super(client);
+
+        StaffAway.find().then(list => {
+            this.staffAwayList = list;
+        }).catch(console.error);
+    }
+
+    findStaffAway(guild: string, id: string, del = false) {
+        let i = 0;
+
+        for (const entry of this.staffAwayList) {
+            if (entry.user === id && entry.guild_id === guild) {
+                if (del) {
+                    this.staffAwayList.splice(i, 1);
+                }
+
+                return entry;
+            }
+
+            i++;
+        }
+
+        return null;
+    }
+
+    async checkStaffAway(message: Message) {
+        if (!message.author.bot && 
+            this.client.config.props[message.guildId!].staffchannel_group && 
+            message.channel.type === 'GUILD_TEXT' && 
+            message.channel.parent?.id && 
+            message.channel.parent?.id === this.client.config.props[message.guildId!].staffchannel_group && 
+            message.member?.roles.cache.has(this.client.config.props[message.guildId!].mod_role)) {
+            const entry = this.findStaffAway(message.guildId!, message.author.id);
+
+            if (entry) {
+                await message.reply(`${userMention(entry.id)} has taken a break from moderating.`);
+            }
+        }
+    }
 }
