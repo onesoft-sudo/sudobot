c5a49e05c98d45f32d62656d4b8070d0d0ce31c2
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-07-08 16:10:16 +0600

    feat(utils): add stringToTimeInterval() function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,51 @@
 export function isSnowflake(input: string) {
     return /^\d{16,22}$/.test(input);
 }
+
+export function stringToTimeInterval(input: string) {
+    let seconds = 0;
+    let number = '';
+
+    for (let i = 0; i < input.length; i++) {
+        if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'].includes(input[i])) {
+            number += input[i];
+        }
+        else {
+            const unit = input[i];
+            const float = parseFloat(number);
+
+            if (isNaN(float)) {
+                return { error: "Invalid numeric time value", seconds: NaN };
+            }
+
+            if (unit === 's') {
+                seconds += float;
+            }
+            else if (unit === 'm') {
+                seconds += float * 60;
+            }
+            else if (unit === 'h') {
+                seconds += float * 60 * 60;
+            }
+            else if (unit === 'd') {
+                seconds += float * 60 * 60 * 24;
+            }
+            else if (unit === 'w') {
+                seconds += float * 60 * 60 * 24 * 7;
+            }
+            else if (unit === 'M') {
+                seconds += float * 60 * 60 * 24 * 30;
+            }
+            else if (unit === 'y') {
+                seconds += float * 60 * 60 * 24 * 365;
+            }
+            else {
+                return { error: "Invalid time unit", seconds: NaN };
+            }
+
+            number = '';
+        }
+    }
+
+    return { error: undefined, seconds };
+}
