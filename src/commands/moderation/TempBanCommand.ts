1a9eba2f541325cdcccd848804057321b33f9a14
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-07-18 21:07:05 +0600

    feat: add tempban command

---
+++ src/commands/moderation/TempBanCommand.ts
@@ -0,0 +1,159 @@
+/*
+ * This file is part of SudoBot.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { formatDistanceToNow, formatDistanceToNowStrict } from "date-fns";
+import { ChatInputCommandInteraction, PermissionsBitField, User, escapeMarkdown } from "discord.js";
+import Command, { AnyCommandContext, ArgumentType, CommandMessage, CommandReturn, ValidationRule } from "../../core/Command";
+import { createModerationEmbed, stringToTimeInterval } from "../../utils/utils";
+import { log } from "../../utils/logger";
+
+export default class BanCommand extends Command {
+    public readonly name = "tempban";
+    public readonly validationRules: ValidationRule[] = [
+        {
+            types: [ArgumentType.User],
+            entityNotNull: true,
+            requiredErrorMessage: "You must specify a user to ban!",
+            typeErrorMessage: "You have specified an invalid user mention or ID.",
+            entityNotNullErrorMessage: "The given user does not exist!",
+            name: "user"
+        },
+        {
+            types: [ArgumentType.TimeInterval],
+            typeErrorMessage: "You have specified an invalid argument. The system expected you to provide a duration here.",
+            requiredErrorMessage: "Please specify a ban duration!",
+            lengthMax: 3999,
+            name: "duration",
+            timeMilliseconds: true
+        },
+        {
+            types: [ArgumentType.TimeInterval, ArgumentType.StringRest],
+            optional: true,
+            typeErrorMessage: "You have specified an invalid argument. The system expected you to provide a ban reason or the message deletion timeframe here.",
+            lengthMax: 3999,
+            minValue: 0,
+            maxValue: 604800,
+            name: 'timeframeOrReason',
+            minMaxErrorMessage: "The message deletion range must be a time interval from 0 second to 604800 seconds (7 days).",
+        },
+        {
+            types: [ArgumentType.StringRest],
+            optional: true,
+            typeErrorMessage: "You have specified an invalid ban reason.",
+            lengthMax: 3999,
+            name: 'reason'
+        }
+    ];
+    public readonly permissions = [PermissionsBitField.Flags.BanMembers];
+
+    async execute(message: CommandMessage, context: AnyCommandContext): Promise<CommandReturn> {
+        await this.deferIfInteraction(message);
+
+        const user: User = context.isLegacy ? context.parsedNamedArgs.user : context.options.getUser("user", true);
+
+        let duration = !context.isLegacy ? undefined : context.parsedNamedArgs.duration;
+        let messageDeletionTimeframe = !context.isLegacy ? undefined : (
+            typeof context.parsedNamedArgs.timeframeOrReason === 'number' ? context.parsedNamedArgs.timeframeOrReason : undefined
+        );
+        const reason = !context.isLegacy ? context.options.getString('reason') ?? undefined : (
+            typeof context.parsedNamedArgs.timeframeOrReason === 'string' ? context.parsedNamedArgs.timeframeOrReason : context.parsedNamedArgs.reason
+        );
+
+        log(user.id, duration, messageDeletionTimeframe, reason);
+
+        if (!context.isLegacy) {
+            const input = context.options.getString('duration', true);
+
+            const { result, error } = stringToTimeInterval(input, { milliseconds: true });
+
+            if (error) {
+                await this.deferredReply(message, {
+                    content: `${this.emoji('error')} ${error} provided in the \`duration\` option`
+                });
+
+                return;
+            }
+
+            duration = result;
+        }
+
+        ifContextIsNotLegacy:
+        if (!context.isLegacy) {
+            const input = context.options.getString('deletion_timeframe');
+
+            if (!input)
+                break ifContextIsNotLegacy;
+
+            const { result, error } = stringToTimeInterval(input);
+
+            if (error) {
+                await this.deferredReply(message, {
+                    content: `${this.emoji('error')} ${error} provided in the \`deletion_timeframe\` option`
+                });
+
+                return;
+            }
+
+            if (result < 0 || result > 604800) {
+                await this.deferredReply(message, `${this.emoji('error')} The message deletion range must be a time interval from 0 second to 604800 seconds (7 days).`);
+                return;
+            }
+
+            messageDeletionTimeframe = result;
+        }
+
+        const id = await this.client.infractionManager.createUserBan(user, {
+            guild: message.guild!,
+            moderator: message.member!.user as User,
+            deleteMessageSeconds: messageDeletionTimeframe,
+            reason,
+            notifyUser: context.isLegacy ? true : !context.options.getBoolean('silent'),
+            sendLog: true,
+            duration,
+            autoRemoveQueue: true
+        });
+
+        if (!id) {
+            await this.error(message);
+            return;
+        }
+
+        await this.deferredReply(message, {
+            embeds: [
+                await createModerationEmbed({
+                    user,
+                    actionDoneName: "banned",
+                    description: `**${escapeMarkdown(user.tag)}** was temporarily banned from this server.`,
+                    fields: [
+                        {
+                            name: 'Message Deletion',
+                            value: messageDeletionTimeframe ? `Timeframe: ${formatDistanceToNow(new Date(Date.now() - (messageDeletionTimeframe * 1000)))}\nMessages in this timeframe by this user will be removed.` : "*No message will be deleted*"
+                        },
+                        {
+                            name: "Duration",
+                            value: formatDistanceToNowStrict(new Date(Date.now() - duration!))
+                        }
+                    ],
+                    id: `${id}`,
+                    reason,
+                })
+            ]
+        });
+    }
+}
