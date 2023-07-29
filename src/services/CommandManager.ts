3c7e4139d2237a2854bdd2e9cfc3498971289c8d
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-07-29 22:17:22 +0600

    refactor: do not throw error if the message content is empty

--- src/services/CommandManager.ts
+++ src/services/CommandManager.ts
@@ -1,25 +1,25 @@
 /**
-* This file is part of SudoBot.
-* 
-* Copyright (C) 2021-2023 OSN Developers.
-*
-* SudoBot is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Affero General Public License as published by 
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* SudoBot is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of 
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License 
-* along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * This file is part of SudoBot.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import { ChatInputCommandInteraction, Message } from "discord.js";
 import Service from "../core/Service";
-import { logError, logWarn, log } from "../utils/logger";
+import { log, logError, logWarn } from "../utils/logger";
 import { GuildConfig } from "./ConfigManager";
 
 export const name = "commandManager";
@@ -45,8 +45,7 @@ export interface ChatInputCommandContext extends CommandContext {
 
 export default class CommandManager extends Service {
     public async runCommandFromMessage(message: Message) {
-        if (!message.content)
-            throw new Error("Invalid message content");
+        if (!message.content) return;
 
         const config = this.client.configManager.config[message.guildId!];
 
@@ -55,12 +54,10 @@ export default class CommandManager extends Service {
             return;
         }
 
-        if (!message.content.startsWith(config.prefix))
-            return;
+        if (!message.content.startsWith(config.prefix)) return;
 
         const commandText = message.content.substring(config.prefix.length);
-        const [commandName, ...commandArguments] = commandText
-            .split(/ +/);
+        const [commandName, ...commandArguments] = commandText.split(/ +/);
 
         const command = this.client.commands.get(commandName);
 
@@ -69,19 +66,20 @@ export default class CommandManager extends Service {
             return await this.client.snippetManager.onMessageCreate(message, commandName);
         }
 
-        command.run(message, <LegacyCommandContext>{
-            isLegacy: true,
-            argv: [commandName, ...commandArguments],
-            args: commandArguments,
-            config,
-            parsedArgs: [],
-            parsedNamedArgs: {},
-            has(arg: string) {
-                return this.args.includes(arg);
-            },
-        })
+        command
+            .run(message, <LegacyCommandContext>{
+                isLegacy: true,
+                argv: [commandName, ...commandArguments],
+                args: commandArguments,
+                config,
+                parsedArgs: [],
+                parsedNamedArgs: {},
+                has(arg: string) {
+                    return this.args.includes(arg);
+                }
+            })
             .then(result => {
-                if (result && typeof result === 'object' && "__reply" in result && result.__reply === true) {
+                if (result && typeof result === "object" && "__reply" in result && result.__reply === true) {
                     message.reply(result as any).catch(console.error);
                 }
             })
@@ -105,13 +103,14 @@ export default class CommandManager extends Service {
             return false;
         }
 
-        command.run(interaction, {
-            isLegacy: false,
-            config,
-            options: interaction.options
-        })
+        command
+            .run(interaction, {
+                isLegacy: false,
+                config,
+                options: interaction.options
+            })
             .then(result => {
-                if (result && typeof result === 'object' && "__reply" in result && result.__reply === true) {
+                if (result && typeof result === "object" && "__reply" in result && result.__reply === true) {
                     interaction.reply(result as any).catch(console.error);
                 }
             })
