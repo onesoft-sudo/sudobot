13532ba48428c91d4eca16aa87a9c17f2d27171d
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-08-01 23:59:57 +0600

    fix: eval command showing backticks when there's no output

--- src/commands/settings/EvalCommand.ts
+++ src/commands/settings/EvalCommand.ts
@@ -1,21 +1,21 @@
 /**
-* This file is part of SudoBot.
-* 
-* Copyright (C) 2021-2023 OSN Developers.
-*
-* SudoBot is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Affero General Public License as published by 
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* SudoBot is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of 
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License 
-* along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * This file is part of SudoBot.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import { SlashCommandBuilder, escapeCodeBlock, escapeMarkdown } from "discord.js";
 import Command, { AnyCommandContext, ArgumentType, CommandMessage, CommandReturn, ValidationRule } from "../../core/Command";
@@ -28,7 +28,7 @@ export default class EvalCommand extends Command {
             types: [ArgumentType.StringRest],
             optional: false,
             name: "code",
-            requiredErrorMessage: "You must provide expression(s) to evaluate!",
+            requiredErrorMessage: "You must provide expression(s) to evaluate!"
         }
     ];
     public readonly systemAdminOnly = true;
@@ -36,14 +36,13 @@ export default class EvalCommand extends Command {
 
     public readonly description = "Execute JavaScript code.";
     public readonly detailedDscription = "This command executes arbitrary JavaScript code. Must be used with caution.";
-    public readonly argumentSyntaxes = [
-        "<...Code>",
-    ];
+    public readonly argumentSyntaxes = ["<...Code>"];
 
     public readonly botRequiredPermissions = [];
 
-    public readonly slashCommandBuilder = new SlashCommandBuilder()
-        .addStringOption(option => option.setName('code').setDescription("The code to execute").setRequired(true));
+    public readonly slashCommandBuilder = new SlashCommandBuilder().addStringOption(option =>
+        option.setName("code").setDescription("The code to execute").setRequired(true)
+    );
 
     createUncaughtExecptionHandler(message: CommandMessage) {
         return (e: Error) => {
@@ -53,7 +52,7 @@ export default class EvalCommand extends Command {
             this.deferredReply(message, {
                 embeds: [
                     {
-                        description: `${this.emoji('error')} **Exception occurred**\n\n\`\`\`\n${escapeMarkdown(e.message + "\n" + e.stack)}\n\`\`\``,
+                        description: `${this.emoji("error")} **Exception occurred**\n\n\`\`\`\n${escapeMarkdown(e.message + "\n" + e.stack)}\n\`\`\``,
                         color: 0xf14a60
                     }
                 ]
@@ -69,7 +68,11 @@ export default class EvalCommand extends Command {
             this.deferredReply(message, {
                 embeds: [
                     {
-                        description: `${this.emoji('error')} **Unhandled promise rejection**\n\n\`\`\`\n${typeof e === 'string' || typeof (e as any)?.toString === 'function' ? escapeCodeBlock((e as any)?.toString ? (e as any).toString() : (e as any)) : e}\n\`\`\``,
+                        description: `${this.emoji("error")} **Unhandled promise rejection**\n\n\`\`\`\n${
+                            typeof e === "string" || typeof (e as any)?.toString === "function"
+                                ? escapeCodeBlock((e as any)?.toString ? (e as any).toString() : (e as any))
+                                : e
+                        }\n\`\`\``,
                         color: 0xf14a60
                     }
                 ]
@@ -91,26 +94,30 @@ export default class EvalCommand extends Command {
 
         try {
             const result = eval(code);
+            const string = `${
+                typeof result === "string" || typeof result?.toString === "function"
+                    ? escapeCodeBlock((result as any)?.toString ? (result as any).toString() : (result as any))
+                    : result
+            }`;
 
             if (!this.errorOccurred) {
                 this.deferredReply(message, {
                     embeds: [
                         {
-                            description: `${this.emoji('check')} **Execution succeeded**\n\n\`\`\`${typeof result === 'string' || typeof result?.toString === 'function' ? escapeCodeBlock((result as any)?.toString ? (result as any).toString() : (result as any)) : result}\`\`\``,
+                            description: `${this.emoji("check")} **Execution succeeded**\n\n${
+                                string.trim() === "" ? "*No output*" : `\`\`\`${string}\`\`\``
+                            }`,
                             color: 0x007bff
                         }
                     ]
                 });
             }
-        }
-        catch (e) {
+        } catch (e) {
             logError("Evaluation failed");
             logError(e);
 
-            if ("stack" in (e as any) && "message" in (e as any))
-                exceptionHandler(e as any);
-            else
-                rejectionHandler(e);
+            if ("stack" in (e as any) && "message" in (e as any)) exceptionHandler(e as any);
+            else rejectionHandler(e);
         }
 
         process.off("uncaughtExceptionMonitor", exceptionHandler);
