1740e03f2eb4b34161efeeb0e5693b7a6510e384
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-08-02 13:27:59 +0600

    feat: add remind command

--- src/commands/automation/RemindCommand.ts
+++ src/commands/automation/RemindCommand.ts
@@ -1,31 +1,80 @@
 /**
-* This file is part of SudoBot.
-* 
-* Copyright (C) 2021-2023 OSN Developers.
-*
-* SudoBot is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Affero General Public License as published by 
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* SudoBot is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of 
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License 
-* along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * This file is part of SudoBot.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+ */
 
-import { EmbedBuilder, PermissionsBitField } from "discord.js";
+import { formatDistanceToNowStrict } from "date-fns";
+import path from "path";
 import Command, { AnyCommandContext, ArgumentType, CommandMessage, CommandReturn, ValidationRule } from "../../core/Command";
+import QueueEntry from "../../utils/QueueEntry";
+import { stringToTimeInterval } from "../../utils/utils";
 
 export default class RemindCommand extends Command {
     public readonly name = "remind";
-    public readonly validationRules: ValidationRule[] = [];
+    public readonly validationRules: ValidationRule[] = [
+        {
+            types: [ArgumentType.TimeInterval],
+            minValue: 1,
+            minMaxErrorMessage: "Please specify a valid time interval!",
+            requiredErrorMessage: "Please specify after how long I should remind you!",
+            typeErrorMessage: "Please specify a valid time interval!",
+            timeMilliseconds: true,
+            name: "time_interval"
+        },
+        {
+            types: [ArgumentType.StringRest],
+            optional: true,
+            typeErrorMessage: "Please specify a valid reminder message!",
+            name: "message"
+        }
+    ];
     public readonly permissions = [];
 
     async execute(message: CommandMessage, context: AnyCommandContext): Promise<CommandReturn> {
-        
+        let timeInterval = context.isLegacy ? context.parsedNamedArgs.time_interval : context.options.getString("time_interval", true);
+
+        if (!context.isLegacy) {
+            const { error, result } = stringToTimeInterval(timeInterval, {
+                milliseconds: true
+            });
+
+            if (error) {
+                await this.error(message, error);
+                return;
+            }
+
+            timeInterval = result;
+        }
+
+        const reminderMessage: string | undefined = context.isLegacy ? context.parsedNamedArgs.message : context.options.getString("message");
+
+        await this.client.queueManager.add(
+            new QueueEntry({
+                args: [message.member!.user.id, reminderMessage],
+                client: this.client,
+                createdAt: new Date(),
+                filePath: path.resolve(__dirname, "../../queues/ReminderQueue"),
+                guild: message.guild!,
+                name: "ReminderQueue",
+                userId: message.member!.user.id,
+                willRunAt: new Date(Date.now() + timeInterval)
+            })
+        );
+
+        await this.success(message, `The system will remind you in ${formatDistanceToNowStrict(new Date(Date.now() - timeInterval))}.`);
     }
 }
---
+++ src/queues/ReminderQueue.ts
@@ -0,0 +1,55 @@
+/*
+ * This file is part of SudoBot.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { formatDistanceToNowStrict } from "date-fns";
+import { EmbedBuilder } from "discord.js";
+import Queue from "../utils/Queue";
+import { log, logError } from "../utils/logger";
+
+export default class ReminderQueue extends Queue {
+    async run(userId: string, message?: string) {
+        try {
+            log("Reminding user");
+
+            const user = this.client.users.cache.get(userId) ?? (await this.client.users.fetch(userId));
+
+            if (!user) {
+                throw new Error("User is null | undefined");
+            }
+
+            await user.send({
+                embeds: [
+                    new EmbedBuilder({
+                        color: 0x007bff,
+                        author: {
+                            name: "Reminder Notification",
+                            icon_url: this.client.user?.displayAvatarURL()
+                        },
+                        description: message ?? "*No message specified*",
+                        footer: {
+                            text: `You had set this reminder ${formatDistanceToNowStrict(this.createdAt, { addSuffix: true })}`
+                        }
+                    }).setTimestamp()
+                ]
+            });
+        } catch (e) {
+            logError(e);
+        }
+    }
+}
