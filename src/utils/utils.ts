5ccbec20a78bebc0f471f1dc6b2635d2d866a212
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-08-09 09:04:42 +0600

    feat(timeparsing): add support of other time units

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -50,32 +50,32 @@ export function stringToTimeInterval(input: string, { milliseconds = false } = {
         if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."].includes(input[i])) {
             number += input[i];
         } else {
-            const unit = input[i];
-            const float = parseFloat(number);
+            const unit = input.substring(i);
+            const float = parseFloat(number.toString());
 
-            if (isNaN(float)) {
+            if (Number.isNaN(float)) {
                 return { error: "Invalid numeric time value", result: NaN };
             }
 
-            if (unit === "s") {
+            if (["s", "sec", "secs", "second", "seconds"].includes(`${unit}`)) {
                 seconds += float;
-            } else if (unit === "m") {
+            } else if (["m", "min", "mins", "minute", "minutes"].includes(`${unit}`)) {
                 seconds += float * 60;
-            } else if (unit === "h") {
+            } else if (["h", "hr", "hrs", "hour", "hours"].includes(unit)) {
                 seconds += float * 60 * 60;
-            } else if (unit === "d") {
+            } else if (["d", "dy", "dys", "day", "days"].includes(unit)) {
                 seconds += float * 60 * 60 * 24;
-            } else if (unit === "w") {
+            } else if (["w", "wk", "wks", "week", "weeks"].includes(unit)) {
                 seconds += float * 60 * 60 * 24 * 7;
-            } else if (unit === "M") {
+            } else if (["M", "mo", "mos", "month", "months"].includes(unit)) {
                 seconds += float * 60 * 60 * 24 * 30;
-            } else if (unit === "y") {
+            } else if (["y", "yr", "yrs", "year", "years"].includes(unit)) {
                 seconds += float * 60 * 60 * 24 * 365;
             } else {
                 return { error: "Invalid time unit", result: NaN };
             }
 
-            number = "";
+            break;
         }
     }
 
