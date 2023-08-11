c997f52cc2f757300c0bc3da2c2a40f590df566a
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-08-11 08:41:09 +0600

    feat: add queue add command

---
+++ src/commands/automation/QueueAddCommand.ts
@@ -0,0 +1,129 @@
+/**
+ * This file is part of SudoBot.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { formatDistanceToNowStrict } from "date-fns";
+import { ChatInputCommandInteraction, EmbedBuilder, Message, PermissionsBitField, escapeMarkdown } from "discord.js";
+import path from "path";
+import Command, { BasicCommandContext, CommandMessage, CommandReturn, ValidationRule } from "../../core/Command";
+import QueueEntry from "../../utils/QueueEntry";
+import { stringToTimeInterval } from "../../utils/utils";
+
+export default class QueueAddCommand extends Command {
+    public readonly name = "queue__add";
+    public readonly validationRules: ValidationRule[] = [];
+    public readonly aliases = ["queueadd", "addqueue", "createqueue"];
+    public readonly permissions = [PermissionsBitField.Flags.ManageMessages];
+    public readonly description = "Creates a new command queue";
+    public readonly since = "5.57.0";
+
+    async execute(message: CommandMessage, context: BasicCommandContext): Promise<CommandReturn> {
+        const deferredMessage = await (await this.deferIfInteraction(message))?.fetch();
+        const runAfterString = context.isLegacy ? context.args[0] : context.options.getString("run_after", true);
+
+        if (!runAfterString) {
+            await this.error(message, "Please specify after how much time the queue should run!");
+            return;
+        }
+
+        const { error, result: runAfter } = stringToTimeInterval(runAfterString, { milliseconds: true });
+
+        if (error) {
+            await this.error(message, error);
+            return;
+        }
+
+        if (context.isLegacy && !context.args[1]) {
+            await this.error(message, "Please specify the command to queue!");
+            return;
+        }
+
+        const commandString = context.isLegacy
+            ? (message as Message).content
+                  .substring(this.client.configManager.config[message.guildId!]?.prefix?.length ?? 1)
+                  .trimStart()
+                  .substring(context.argv[0] === "queue" ? "queue".length : 0)
+                  .trimStart()
+                  .substring(context.argv[0] === "queue" ? this.name.replace("queue__", "").length : context.argv[0].length)
+                  .trimStart()
+                  .substring(runAfterString.length)
+                  .trim()
+            : context.options.getString("command", true).trim();
+
+        let commandName = "",
+            content = "";
+
+        const prefix = this.client.configManager.config[message.guildId!]?.prefix ?? "-";
+
+        if (message instanceof Message) {
+            content = message.content;
+            message.content = prefix + commandString;
+        } else {
+            commandName = message.commandName;
+            message.commandName = commandString.split(/ +/)[0];
+        }
+
+        const result = context.isLegacy
+            ? await this.client.commandManager.runCommandFromMessage(message as Message, true)
+            : await this.client.commandManager.runCommandFromCommandInteraction(message as ChatInputCommandInteraction, true);
+
+        if (message instanceof Message) {
+            message.content = content;
+        } else {
+            message.commandName = commandName;
+        }
+
+        if (result === false) {
+            await this.error(message, `The command you've specified could not be found. Please check for spelling errors.`);
+            return;
+        }
+
+        const id = await this.client.queueManager.add(
+            new QueueEntry({
+                args: [message.channelId!, message instanceof Message ? message.id : deferredMessage!.id, commandString],
+                client: this.client,
+                createdAt: new Date(),
+                filePath: path.resolve(__dirname, "../../queues/CommandQueue"),
+                guild: message.guild!,
+                name: "CommandQueue",
+                userId: message.member!.user.id,
+                willRunAt: new Date(Date.now() + runAfter)
+            })
+        );
+
+        await this.deferredReply(message, {
+            embeds: [
+                new EmbedBuilder({
+                    title: "Queue created",
+                    color: 0x007bff,
+                    description: `${this.emoji(
+                        "check"
+                    )} Successfully added the queue. The following command will be run after **${formatDistanceToNowStrict(
+                        new Date(Date.now() - runAfter)
+                    )}**:\n\n\`\`\`${escapeMarkdown(prefix + commandString)}\`\`\``,
+                    fields: [
+                        {
+                            name: "Queue ID",
+                            value: id.toString()
+                        }
+                    ]
+                }).setTimestamp()
+            ]
+        });
+    }
+}
---
+++ src/commands/automation/QueueCommand.ts
@@ -0,0 +1,67 @@
+/**
+ * This file is part of SudoBot.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { PermissionsBitField, SlashCommandBuilder } from "discord.js";
+import Command, { ArgumentType, BasicCommandContext, CommandMessage, CommandReturn, ValidationRule } from "../../core/Command";
+
+export default class QueueCommand extends Command {
+    public readonly subcommands = ["add"];
+    public readonly name = "queue";
+    public readonly validationRules: ValidationRule[] = [
+        {
+            types: [ArgumentType.String],
+            requiredErrorMessage: `Please provide a subcommand! The valid commands are: \`${this.subcommands.join("`, `")}\``,
+            typeErrorMessage: "Please provide a valid subcommand!",
+            name: "subcommand"
+        }
+    ];
+    public readonly permissions = [PermissionsBitField.Flags.ManageMessages];
+    public readonly description = "Manage the queued jobs";
+    public readonly since = "5.57.0";
+    public readonly slashCommandBuilder = new SlashCommandBuilder().addSubcommand(subcommand =>
+        subcommand
+            .setName("add")
+            .setDescription("Creates a new command queue")
+            .addStringOption(option =>
+                option
+                    .setName("run_after")
+                    .setDescription("Specify after how much time the queued job will run")
+                    .setRequired(true)
+            )
+            .addStringOption(option =>
+                option
+                    .setName("command")
+                    .setDescription("Specify what command to run (include everything just without the prefix)")
+                    .setRequired(true)
+            )
+    );
+
+    async execute(message: CommandMessage, context: BasicCommandContext): Promise<CommandReturn> {
+        const subcommand = context.isLegacy ? context.parsedNamedArgs.subcommand : context.options.getSubcommand(true);
+        const command = this.client.commands.get(`queue__${subcommand}`);
+
+        if (!command) {
+            return;
+        }
+
+        if (context.isLegacy) context.args.shift();
+
+        return await command.execute(message, context);
+    }
+}
