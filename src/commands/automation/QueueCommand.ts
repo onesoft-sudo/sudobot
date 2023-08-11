100fa98bcb11c62085b5fac3c15b18e2ae34160f
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-08-11 09:18:25 +0600

    fix(commands): update the parent queue command

--- src/commands/automation/QueueCommand.ts
+++ src/commands/automation/QueueCommand.ts
@@ -21,12 +21,14 @@ import { PermissionsBitField, SlashCommandBuilder } from "discord.js";
 import Command, { ArgumentType, BasicCommandContext, CommandMessage, CommandReturn, ValidationRule } from "../../core/Command";
 
 export default class QueueCommand extends Command {
-    public readonly subcommands = ["add"];
+    public readonly subcommands = ["add", "cancel", "list", "show"];
     public readonly name = "queue";
     public readonly validationRules: ValidationRule[] = [
         {
             types: [ArgumentType.String],
-            requiredErrorMessage: `Please provide a subcommand! The valid commands are: \`${this.subcommands.join("`, `")}\``,
+            requiredErrorMessage: `Please provide a subcommand! The valid commands are: \`${this.subcommands.join(
+                "`, `"
+            )}\`, \`remove\`, \`view\``,
             typeErrorMessage: "Please provide a valid subcommand!",
             name: "subcommand"
         }
@@ -34,23 +36,37 @@ export default class QueueCommand extends Command {
     public readonly permissions = [PermissionsBitField.Flags.ManageMessages];
     public readonly description = "Manage the queued jobs";
     public readonly since = "5.57.0";
-    public readonly slashCommandBuilder = new SlashCommandBuilder().addSubcommand(subcommand =>
-        subcommand
-            .setName("add")
-            .setDescription("Creates a new command queue")
-            .addStringOption(option =>
-                option
-                    .setName("run_after")
-                    .setDescription("Specify after how much time the queued job will run")
-                    .setRequired(true)
-            )
-            .addStringOption(option =>
-                option
-                    .setName("command")
-                    .setDescription("Specify what command to run (include everything just without the prefix)")
-                    .setRequired(true)
-            )
-    );
+    public readonly slashCommandBuilder = new SlashCommandBuilder()
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("add")
+                .setDescription("Creates a new command queue")
+                .addStringOption(option =>
+                    option
+                        .setName("run_after")
+                        .setDescription("Specify after how much time the queued job will run")
+                        .setRequired(true)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName("command")
+                        .setDescription("Specify what command to run (include everything just without the prefix)")
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("cancel")
+                .setDescription("Cancels a previously added queue")
+                .addIntegerOption(option => option.setName("id").setDescription("The queue ID").setRequired(true))
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("show")
+                .setDescription("Shows information about a previously added queue")
+                .addIntegerOption(option => option.setName("id").setDescription("The queue ID").setRequired(true))
+        )
+        .addSubcommand(subcommand => subcommand.setName("list").setDescription("List all queued jobs"));
 
     async execute(message: CommandMessage, context: BasicCommandContext): Promise<CommandReturn> {
         const subcommand = context.isLegacy ? context.parsedNamedArgs.subcommand : context.options.getSubcommand(true);
