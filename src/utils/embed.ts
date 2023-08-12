41f27abe2324b69e43e94c27d15a013709708a9b
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-08-12 20:47:57 +0600

    feat(utils): helper functions for formatting information in command responses

--- src/utils/embed.ts
+++ src/utils/embed.ts
@@ -1,23 +1,23 @@
 /*
-* This file is part of SudoBot.
-*
-* Copyright (C) 2021-2023 OSN Developers.
-*
-* SudoBot is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SudoBot is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * This file is part of SudoBot.
+ *
+ * Copyright (C) 2021-2023 OSN Developers.
+ *
+ * SudoBot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+ */
 
-import { ColorResolvable, EmbedBuilder, resolveColor } from "discord.js";
+import { Channel, ChannelType, ColorResolvable, EmbedBuilder, Message, User, resolveColor } from "discord.js";
 import { ChatInputCommandContext } from "../services/CommandManager";
 
 export function generateEmbed(options: ChatInputCommandContext["options"]) {
@@ -35,7 +35,10 @@ export function generateEmbed(options: ChatInputCommandContext["options"]) {
         iconURL: getString("footer_iconurl")
     };
 
-    if (getString("color") && (!resolveColor(getString("color") as ColorResolvable) || isNaN(resolveColor(getString("color") as ColorResolvable)))) {
+    if (
+        getString("color") &&
+        (!resolveColor(getString("color") as ColorResolvable) || isNaN(resolveColor(getString("color") as ColorResolvable)))
+    ) {
         return { error: "Invalid color given." };
     }
 
@@ -59,7 +62,11 @@ export function generateEmbed(options: ChatInputCommandContext["options"]) {
               }
             : undefined,
         footer: footer.text ? footer : undefined,
-        timestamp: getString("timestamp") ? (getString("timestamp") === "current" ? new Date() : new Date(getString("timestamp")!)) : undefined,
+        timestamp: getString("timestamp")
+            ? getString("timestamp") === "current"
+                ? new Date()
+                : new Date(getString("timestamp")!)
+            : undefined,
         fields: getString("fields")
             ? getString("fields")!
                   .trim()
@@ -77,4 +84,16 @@ export function generateEmbed(options: ChatInputCommandContext["options"]) {
     }).setColor((getString("color") ?? "#007bff") as ColorResolvable);
 
     return { embed };
+}
+
+export function userInfo(user: User) {
+    return `ID: ${user.id}\nUsername: ${user.username}\nMention: ${user.toString()}`;
+}
+
+export function messageInfo(message: Message) {
+    return `ID: ${message.id}\nURL: ${message.url}`;
+}
+
+export function channelInfo(channel: Channel) {
+    return `ID: ${channel.id}\nType: ${ChannelType[channel.type]}\nMention: ${channel.toString()}`;
 }
