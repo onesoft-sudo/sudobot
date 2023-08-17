145cf8e23fbefe59438424bccd2681be04fcf76f
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-08-17 17:02:08 +0600

    feat(logging): member kick logs

--- src/events/guildMember/GuildMemberRemoveEvent.ts
+++ src/events/guildMember/GuildMemberRemoveEvent.ts
@@ -17,13 +17,52 @@
  * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { ClientEvents, GuildMember } from "discord.js";
+import { InfractionType } from "@prisma/client";
+import { AuditLogEvent, ClientEvents, GuildMember } from "discord.js";
 import Event from "../../core/Event";
+import { logError } from "../../utils/logger";
 
 export default class GuildMemberRemoveEvent extends Event {
     public name: keyof ClientEvents = "guildMemberRemove";
 
     async execute(member: GuildMember) {
         await this.client.logger.logGuildMemberRemove(member);
+
+        setTimeout(async () => {
+            try {
+                const auditLog = (
+                    await member.guild.fetchAuditLogs({
+                        limit: 1,
+                        type: AuditLogEvent.MemberKick
+                    })
+                ).entries.first();
+
+                if (
+                    auditLog?.executor?.id &&
+                    auditLog.executor.id !== this.client.user?.id &&
+                    auditLog.targetId === member.user.id
+                ) {
+                    const infraction = await this.client.prisma.infraction.create({
+                        data: {
+                            guildId: member.guild.id,
+                            moderatorId: auditLog.executor.id,
+                            type: InfractionType.KICK,
+                            userId: member.user.id,
+                            reason: auditLog.reason ?? undefined
+                        }
+                    });
+
+                    await this.client.logger.logMemberKick({
+                        moderator: auditLog.executor,
+                        id: infraction.id.toString(),
+                        reason: auditLog.reason ?? undefined,
+                        guild: member.guild,
+                        member
+                    });
+                }
+            } catch (e) {
+                logError(e);
+            }
+        }, 2000);
     }
 }
