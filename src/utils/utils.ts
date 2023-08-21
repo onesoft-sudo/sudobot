5042cfa30cbece36bfeda61160088e95dd46e0f9
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-08-21 08:58:49 +0600

    refactor(utils): move datetime related functions to a separate file

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -42,46 +42,6 @@ export function isSnowflake(input: string) {
     return /^\d{16,22}$/.test(input);
 }
 
-export function stringToTimeInterval(input: string, { milliseconds = false } = {}) {
-    let seconds = 0;
-    let number = "";
-
-    for (let i = 0; i < input.length; i++) {
-        if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."].includes(input[i])) {
-            number += input[i];
-        } else {
-            const unit = input.substring(i);
-            const float = parseFloat(number.toString());
-
-            if (Number.isNaN(float)) {
-                return { error: "Invalid numeric time value", result: NaN };
-            }
-
-            if (["s", "sec", "secs", "second", "seconds"].includes(`${unit}`)) {
-                seconds += float;
-            } else if (["m", "min", "mins", "minute", "minutes"].includes(`${unit}`)) {
-                seconds += float * 60;
-            } else if (["h", "hr", "hrs", "hour", "hours"].includes(unit)) {
-                seconds += float * 60 * 60;
-            } else if (["d", "dy", "dys", "day", "days"].includes(unit)) {
-                seconds += float * 60 * 60 * 24;
-            } else if (["w", "wk", "wks", "week", "weeks"].includes(unit)) {
-                seconds += float * 60 * 60 * 24 * 7;
-            } else if (["M", "mo", "mos", "month", "months"].includes(unit)) {
-                seconds += float * 60 * 60 * 24 * 30;
-            } else if (["y", "yr", "yrs", "year", "years"].includes(unit)) {
-                seconds += float * 60 * 60 * 24 * 365;
-            } else {
-                return { error: "Invalid time unit", result: NaN };
-            }
-
-            break;
-        }
-    }
-
-    return { error: undefined, result: seconds * (milliseconds ? 1000 : 1) };
-}
-
 export interface CreateModerationEmbedOptions {
     user: User;
     actionDoneName?: ActionDoneName;
