d7ae20665b9240b765de9511063cd7e214ed8de6
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-09-20 09:18:46 +0600

    feat(sniping): snipe multiple messages

--- src/commands/moderation/SnipeCommand.ts
+++ src/commands/moderation/SnipeCommand.ts
@@ -18,15 +18,29 @@
  */
 
 import { ChannelType, EmbedBuilder, Message, PartialMessage, PermissionsBitField, Snowflake } from "discord.js";
-import Command, { AnyCommandContext, CommandMessage, CommandReturn, ValidationRule } from "../../core/Command";
+import Command, { AnyCommandContext, ArgumentType, CommandMessage, CommandReturn, ValidationRule } from "../../core/Command";
 import { GatewayEventListener } from "../../decorators/GatewayEventListener";
 import { HasEventListeners } from "../../types/HasEventListeners";
 
 export default class SnipeCommand extends Command implements HasEventListeners {
     public readonly name = "snipe";
-    public readonly validationRules: ValidationRule[] = [];
+    public readonly validationRules: ValidationRule[] = [
+        {
+            types: [ArgumentType.Integer],
+            optional: true,
+            typeErrorMessage: "You must provide a valid message index/number between 1-10!",
+            minMaxErrorMessage: "You must provide a valid message index/number between 1-10!",
+            maxValue: 11,
+            minValue: 1,
+            default: null,
+            name: "index"
+        }
+    ];
     public readonly permissions = [PermissionsBitField.Flags.ManageMessages];
-    protected lastDeletedMessages = new Map<Snowflake, Message<boolean> | PartialMessage | undefined>();
+    public readonly argumentSyntaxes = ["[index=1]"];
+    public readonly aliases = ["clearsnipe", "cs", "delsnipe", "csnipe", "s"];
+    public readonly since: string = "4.4.0";
+    protected readonly lastDeletedMessages = new Map<Snowflake, Array<Message<boolean> | PartialMessage> | undefined>();
 
     public readonly description = "Reposts the last deleted message.";
 
@@ -41,14 +55,47 @@ export default class SnipeCommand extends Command implements HasEventListeners {
             return;
         }
 
-        this.lastDeletedMessages.set(message.guildId!, message);
+        const deletedMessages = this.lastDeletedMessages.get(message.guildId!);
+
+        if (deletedMessages === undefined) {
+            this.lastDeletedMessages.set(message.guildId!, [message]);
+        } else {
+            if (deletedMessages.length > 10) deletedMessages.pop();
+            deletedMessages.unshift(message);
+        }
     }
 
     async execute(message: CommandMessage, context: AnyCommandContext): Promise<CommandReturn> {
-        const lastDeletedMessage = this.lastDeletedMessages.get(message.guildId!);
+        const index = context.isLegacy ? (context.parsedNamedArgs.index ?? 1) - 1 : 0;
+        const lastDeletedMessages = this.lastDeletedMessages.get(message.guildId!);
+        const lastDeletedMessage = lastDeletedMessages?.[index];
+
+        if (lastDeletedMessages?.length && index >= lastDeletedMessages?.length) {
+            await this.error(
+                message,
+                `Invalid message index - only ${lastDeletedMessages.length} message${
+                    lastDeletedMessages.length === 1 ? " is" : "s are"
+                } have been recorded so far.`
+            );
+            return;
+        }
 
         if (!lastDeletedMessage) {
             await this.error(message, `No deleted message was recorded yet.`);
+            return;
+        }
+
+        if (context.isLegacy && context.argv[0] !== "s" && context.argv[0] !== "snipe") {
+            const hasValue = context.parsedNamedArgs.index !== null;
+
+            if (!hasValue) {
+                lastDeletedMessages.splice(0, lastDeletedMessages.length);
+                await this.success(message, "Cleared sniped messages for this server.");
+            } else {
+                lastDeletedMessages.splice(index, 1);
+                await this.success(message, "Removed the given sniped message for this server.");
+            }
+
             return;
         }
 
@@ -63,7 +110,9 @@ export default class SnipeCommand extends Command implements HasEventListeners {
                     color: Math.floor(Math.random() * 0xffffff),
                     description: lastDeletedMessage.content!,
                     footer: {
-                        text: "Sniped"
+                        text: `Sniped • ${lastDeletedMessages.length ?? 0} message${
+                            lastDeletedMessages.length === 1 ? " is" : "s are"
+                        } sniped`
                     }
                 }).setTimestamp()
             ]
