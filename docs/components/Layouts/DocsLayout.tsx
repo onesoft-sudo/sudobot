c85c90faba92cb0a590befe6c0aa710f433bd96b
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-01-11 02:50:26 +0600

    docs: update docs layout

--- docs/components/Layouts/DocsLayout.tsx
+++ docs/components/Layouts/DocsLayout.tsx
@@ -7,6 +7,7 @@ export default function DocsLayout({ children }: PropsWithChildren) {
     return (
         <div className="grid lg:grid-cols-[3fr_10fr_2.5fr] md:gap-[50px] mb-10">
             <DocsLinkList desktopOnly fragment />
+
             <div className="lg:px-[50px] xl:px-[100px]">
                 <article
                     id="article"
