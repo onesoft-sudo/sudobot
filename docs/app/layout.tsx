5ee5fa2dac7b32f833609b4bf968448e233f1295
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-01-12 00:14:56 +0600

    docs: update layout.tsx

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: PropsWithChildren) {
     return (
-        <html lang="en">
+        <html lang="en" className="dark">
             <body className={inter.className}>
                 <Navbar />
                 {children}
