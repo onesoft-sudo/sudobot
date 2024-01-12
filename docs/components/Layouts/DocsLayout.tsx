fb4e4b0aaf75825e2567c1139f3233c6c2884de2
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-01-12 15:13:33 +0600

    docs: fix table of contents

--- docs/components/Layouts/DocsLayout.tsx
+++ docs/components/Layouts/DocsLayout.tsx
@@ -6,10 +6,10 @@ import Navigator from "../Navigation/Navigator";
 
 export default function DocsLayout({ children }: PropsWithChildren) {
     return (
-        <div className="grid lg:grid-cols-[3fr_10fr_2.5fr] md:gap-[50px] mb-10">
+        <div className="grid lg:grid-cols-[3fr_10fr_2.5fr] md:gap-[50px] mb-10 relative">
             <DocsLinkList desktopOnly fragment />
 
-            <div className="lg:px-[50px] xl:px-[100px]">
+            <div className="lg:px-[50px] xl:px-[100px] lg:max-w-[60vw]">
                 <article
                     id="article"
                     className="prose prose-neutral prose-invert prose-code:before:hidden prose-code:after:hidden mt-8 p-3 text-wrap max-w-[100vw]"
@@ -32,7 +32,7 @@ export default function DocsLayout({ children }: PropsWithChildren) {
                 </div>
             </div>
 
-            <div className="hidden lg:block mr-5 fixed right-0 max-h-[calc(100vh-50px)] overflow-y-scroll">
+            <div className="hidden lg:block mr-5 fixed right-0 max-w-[20vw] max-h-[calc(100vh-50px)] overflow-y-scroll">
                 <TableOfContents as={Fragment} />
             </div>
         </div>
