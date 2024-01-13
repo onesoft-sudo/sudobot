a31561bea5071fb566b398494ab70b3062bdcbde
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-01-13 22:30:30 +0600

    docs: update usePlatform.ts

--- docs/hooks/usePlatform.ts
+++ docs/hooks/usePlatform.ts
@@ -1,6 +1,10 @@
 "use client";
 
 export default function usePlatform() {
+    if (typeof navigator === "undefined") {
+        return "darwin";
+    }
+
     return navigator.userAgent.includes("Macintosh")
         ? "darwin"
         : navigator.userAgent.includes("Windows")
