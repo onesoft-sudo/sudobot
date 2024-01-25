dd4517c9645ee1b5b972872fa66919b94ded735c
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-01-25 21:49:05 +0600

    feat: credentials server auth

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,13 @@
 import "module-alias/register";
 import "reflect-metadata";
 
+import axios from "axios";
 import { spawn } from "child_process";
-import { GatewayIntentBits, Options, Partials } from "discord.js";
+import { GatewayIntentBits, Partials } from "discord.js";
 import "dotenv/config";
+import { createInterface } from "node:readline/promises";
 import Client from "./core/Client";
+import { logError, logInfo, logSuccess } from "./utils/logger";
 
 global.bootDate = Date.now();
 
@@ -40,48 +43,6 @@ const intents = [
 
 const partials = [Partials.Channel];
 
-export const client = new Client({
-    intents,
-    partials,
-    makeCache: Options.cacheWithLimits({
-        ...Options.DefaultMakeCacheSettings,
-        ReactionManager: 0,
-        DMMessageManager: 0,
-        GuildForumThreadManager: 0,
-        GuildScheduledEventManager: 0,
-        VoiceStateManager: 0,
-        StageInstanceManager: 0,
-        GuildTextThreadManager: 0,
-        GuildStickerManager: 0,
-        AutoModerationRuleManager: 0,
-        ApplicationCommandManager: 0,
-        ThreadMemberManager: 0,
-        ThreadManager: 0,
-        ReactionUserManager: 0,
-        GuildMemberManager: 25,
-        GuildBanManager: 0,
-        GuildEmojiManager: 25,
-        GuildInviteManager: 5,
-        GuildMessageManager: 50,
-        BaseGuildEmojiManager: 5
-    }),
-    sweepers: {
-        ...Options.DefaultSweeperSettings,
-        messages: {
-            interval: 1200, // Every 20 minutes.
-            lifetime: 600 // Remove messages older than 10 minutes.
-        },
-        users: {
-            interval: 1200,
-            filter: () => user => user.bot && user.id !== user.client.user.id // Remove all bots.
-        },
-        guildMembers: {
-            interval: 1200,
-            filter: () => member => member.user.bot && member.id !== member.client.user.id // Remove all bots.
-        }
-    }
-});
-
 function spawnNativeProcess() {
     const path = process.env.EXPERIMENTAL_NATIVE_EXECUTABLE_PATH;
 
@@ -97,7 +58,84 @@ function spawnNativeProcess() {
     }
 }
 
+async function fetchCredentials() {
+    if (!process.env.CREDENTIAL_SERVER) {
+        return;
+    }
+
+    const readline = createInterface(process.stdin, process.stdout);
+    const key = await readline.question("Enter key to authenticate with the credentials server: ");
+
+    logInfo("Authenticating with the server...");
+
+    try {
+        const response = await axios.get(process.env.CREDENTIAL_SERVER, {
+            headers: {
+                Authorization: `Bearer ${key}`
+            }
+        });
+
+        if (response.data?.success && response.data?.config && typeof response.data?.config === "object") {
+            logSuccess("Successfully authenticated with the credentials server");
+
+            for (const key in response.data.config) {
+                process.env[key] = response.data.config[key];
+            }
+
+            console.log(response.data.config);
+        } else {
+            throw new Error("Invalid response received");
+        }
+    } catch (error) {
+        logError(error);
+        process.exit(-1);
+    }
+}
+
 (async () => {
+    await fetchCredentials();
+    const client = new Client({
+        intents,
+        partials
+        // makeCache: Options.cacheWithLimits({
+        //     ...Options.DefaultMakeCacheSettings,
+        //     ReactionManager: 0,
+        //     DMMessageManager: 0,
+        //     GuildForumThreadManager: 0,
+        //     GuildScheduledEventManager: 0,
+        //     VoiceStateManager: 0,
+        //     StageInstanceManager: 0,
+        //     GuildTextThreadManager: 0,
+        //     GuildStickerManager: 0,
+        //     AutoModerationRuleManager: 0,
+        //     ApplicationCommandManager: 0,
+        //     ThreadMemberManager: 0,
+        //     ThreadManager: 0,
+        //     ReactionUserManager: 0,
+        //     GuildMemberManager: 25,
+        //     GuildBanManager: 0,
+        //     GuildEmojiManager: 25,
+        //     GuildInviteManager: 5,
+        //     GuildMessageManager: 50,
+        //     BaseGuildEmojiManager: 5
+        // }),
+        // sweepers: {
+        //     ...Options.DefaultSweeperSettings,
+        //     messages: {
+        //         interval: 1200, // Every 20 minutes.
+        //         lifetime: 600 // Remove messages older than 10 minutes.
+        //     },
+        //     users: {
+        //         interval: 1200,
+        //         filter: () => user => user.bot && user.id !== user.client.user.id // Remove all bots.
+        //     },
+        //     guildMembers: {
+        //         interval: 1200,
+        //         filter: () => member => member.user.bot && member.id !== member.client.user.id // Remove all bots.
+        //     }
+        // }
+    });
+
     spawnNativeProcess();
 
     await client.boot();
