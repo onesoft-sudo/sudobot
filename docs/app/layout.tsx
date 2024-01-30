85f64cb30ae385cdc7cb578f495975066823a7c6
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-01-31 00:56:35 +0600

    docs: opengraph meta info

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Progress from "@/components/Navigation/Progress";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { PropsWithChildren } from "react";
 import "../styles/globals.css";
@@ -11,6 +11,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "SudoBot Documentation",
     description: "A guide to get you started with SudoBot!",
+};
+
+export const viewport: Viewport = {
+    colorScheme: "dark",
+    initialScale: 1,
+    themeColor: "#007bff",
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
