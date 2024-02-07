be91d7d7082eccda37c98d2a06995fd2214ecad9
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-02-07 12:30:52 +0600

    chore(scripts): index creation support

--- scripts/extensions.js
+++ scripts/extensions.js
@@ -20,13 +20,16 @@
  */
 
 require("module-alias/register");
+require("dotenv/config");
 const chalk = require("chalk");
 const { spawnSync } = require("child_process");
 const { existsSync, lstatSync, readdirSync, readFileSync, writeFileSync, rmSync } = require("fs");
+const { readFile } = require("fs/promises");
 const path = require("path");
 const { chdir, cwd } = require("process");
+const { z } = require("zod");
 
-const extensionsPath = path.resolve(__dirname, "../extensions");
+const extensionsPath = process.env.EXTENSIONS_DIRECTORY ?? path.resolve(__dirname, "../extensions");
 
 function error(...args) {
     console.error(...args);
@@ -38,6 +41,10 @@ if (!existsSync(extensionsPath)) {
 }
 
 function getRecuriveJavaScriptFiles(dir) {
+    if (!existsSync(dir)) {
+        return [];
+    }
+
     const files = readdirSync(dir);
     const flat = [];
 
@@ -60,6 +67,22 @@ function getRecuriveJavaScriptFiles(dir) {
     return flat;
 }
 
+const extensionMetadataSchema = z.object({
+    main: z.string().optional(),
+    commands: z.string().optional(),
+    services: z.string().optional(),
+    events: z.string().optional(),
+    language: z.enum(["typescript", "javascript"]).optional(),
+    main_directory: z.string().optional(),
+    build_command: z.string().optional(),
+    name: z.string().optional(),
+    description: z.string().optional(),
+    resources: z.string().optional(),
+    id: z.string(),
+    icon: z.string().optional(),
+    readmeFileName: z.string().default("README.md")
+});
+
 function readMeta(extensionName, extensionDirectory) {
     const metadataFile = path.join(extensionDirectory, "extension.json");
 
@@ -68,7 +91,12 @@ function readMeta(extensionName, extensionDirectory) {
     }
 
     const metadata = JSON.parse(readFileSync(metadataFile, { encoding: "utf-8" }));
-    return metadata;
+
+    try {
+        return extensionMetadataSchema.parse(metadata);
+    } catch (e) {
+        error(`Extension ${extensionName} has an invalid "extension.json" file!`, e);
+    }
 }
 
 async function writeCacheIndex() {
@@ -80,33 +108,20 @@ async function writeCacheIndex() {
         const extensionDirectory = path.resolve(extensionsPath, extensionName);
         const isDirectory = lstatSync(extensionDirectory).isDirectory();
 
-        if (!isDirectory) {
+        if (!isDirectory || extensionName === ".extbuilds") {
             continue;
         }
 
         console.log("Found extension: ", extensionName);
 
-        // const metadataFile = path.join(extensionDirectory, "extension.json");
-
-        // if (!existsSync(metadataFile)) {
-        //     error(`Extension ${extensionName} does not have a "extension.json" file!`);
-        // }
-
-        // const metadata = JSON.parse(readFileSync(metadataFile, { encoding: "utf-8" }));
-        // const {
-        //     main_directory = "./build",
-        //     commands = `./${main_directory}/commands`,
-        //     events = `./${main_directory}/events`,
-        //     main = `./${main_directory}/index.js`,
-        //     language = "typescript"
-        // } = metadata;
-
         const {
             main_directory = "./build",
             commands = `./${main_directory}/commands`,
             events = `./${main_directory}/events`,
+            services = `./${main_directory}/services`,
             main = `./${main_directory}/index.js`,
-            language = "typescript"
+            language = "typescript",
+            id
         } = readMeta(extensionName, extensionDirectory);
 
         const extensionPath = path.join(extensionDirectory, main);
@@ -116,9 +131,14 @@ async function writeCacheIndex() {
         const extension = new ExtensionClass(this.client);
         let commandPaths = await extension.commands();
         let eventPaths = await extension.events();
+        let servicePaths = await extension.services();
 
         if (commandPaths === null) {
-            const directory = path.join(__dirname, "../extensions", extensionName, commands);
+            const directory = path.join(
+                ...(process.env.EXTENSIONS_DIRECTORY ? [process.env.EXTENSIONS_DIRECTORY] : [__dirname, "../extensions"]),
+                extensionName,
+                commands
+            );
 
             if (existsSync(directory)) {
                 commandPaths = getRecuriveJavaScriptFiles(directory);
@@ -126,10 +146,26 @@ async function writeCacheIndex() {
         }
 
         if (eventPaths === null) {
-            const directory = path.join(__dirname, "../extensions", extensionName, events);
+            const directory = path.join(
+                ...(process.env.EXTENSIONS_DIRECTORY ? [process.env.EXTENSIONS_DIRECTORY] : [__dirname, "../extensions"]),
+                extensionName,
+                events
+            );
 
             if (existsSync(directory)) {
                 eventPaths = getRecuriveJavaScriptFiles(directory);
+            }
+        }
+
+        if (servicePaths === null) {
+            const directory = path.join(
+                ...(process.env.EXTENSIONS_DIRECTORY ? [process.env.EXTENSIONS_DIRECTORY] : [__dirname, "../extensions"]),
+                extensionName,
+                services
+            );
+
+            if (existsSync(directory)) {
+                servicePaths = getRecuriveJavaScriptFiles(directory);
             }
         }
 
@@ -137,7 +173,9 @@ async function writeCacheIndex() {
             name: extensionName,
             entry: extensionPath,
             commands: commandPaths ?? [],
-            events: eventPaths ?? []
+            events: eventPaths ?? [],
+            services: servicePaths ?? [],
+            id
         });
 
         meta.push({
@@ -149,14 +187,15 @@ async function writeCacheIndex() {
     console.table(
         extensionsOutputArray.map((e, i) => ({
             name: e.name,
-            entry: e.entry.replace(path.resolve(__dirname, "../extensions"), "{ROOT}"),
+            entry: e.entry.replace(extensionsPath, "{ROOT}"),
             commands: e.commands.length,
             events: e.events.length,
+            services: e.services.length,
             language: meta[i].language
         }))
     );
 
-    const indexFile = path.join(__dirname, "../extensions/index.json");
+    const indexFile = path.join(extensionsPath, "index.json");
 
     writeFileSync(
         indexFile,
@@ -199,7 +238,7 @@ async function buildExtensions() {
         const extensionDirectory = path.resolve(extensionsPath, extensionName);
         const isDirectory = lstatSync(extensionDirectory).isDirectory();
 
-        if (!isDirectory) {
+        if (!isDirectory || extensionName === ".extbuilds") {
             continue;
         }
 
@@ -213,7 +252,7 @@ async function buildExtensions() {
         });
 
         actionLog("RELINK", extensionName);
-        spawnSyncCatchExit(`npm link --save ${path.relative(cwd(), path.resolve(__dirname, ".."))}`, {
+        spawnSyncCatchExit(`npm install --save ${path.relative(cwd(), path.resolve(__dirname, ".."))}`, {
             encoding: "utf-8",
             shell: true,
             stdio: "inherit"
@@ -238,8 +277,91 @@ async function buildExtensions() {
     chdir(workingDirectory);
 }
 
+async function writeExtensionIndex() {
+    const extensionsPath = path.resolve(__dirname, "../extensions");
+    const extensionsOutputRecord = {};
+    const extensions = readdirSync(extensionsPath);
+
+    for await (const extensionName of extensions) {
+        const extensionDirectory = path.resolve(extensionsPath, extensionName);
+        const isDirectory = lstatSync(extensionDirectory).isDirectory();
+
+        if (!isDirectory || extensionName === ".extbuilds") {
+            continue;
+        }
+
+        console.log("Found extension: ", extensionName);
+
+        const packageJsonPath = path.join(extensionDirectory, "package.json");
+        const packageJson = JSON.parse(await readFile(packageJsonPath, { encoding: "utf-8" }));
+
+        const {
+            main_directory = "./build",
+            commands = `./${main_directory}/commands`,
+            events = `./${main_directory}/events`,
+            build_command = null,
+            name = packageJson.name ?? extensionName,
+            description = packageJson.description,
+            language = "javascript",
+            main = `./${main_directory}/index.js`,
+            services = `./${main_directory}/services`,
+            id,
+            icon,
+            readmeFileName
+        } = readMeta(extensionName, extensionDirectory) ?? {};
+
+        const commandPaths = getRecuriveJavaScriptFiles(path.join(extensionDirectory, commands));
+        const eventPaths = getRecuriveJavaScriptFiles(path.join(extensionDirectory, events));
+        const servicePaths = getRecuriveJavaScriptFiles(path.join(extensionDirectory, services));
+
+        extensionsOutputRecord[id] = {
+            name,
+            id,
+            description,
+            commands: commandPaths.length,
+            events: eventPaths.length,
+            buildCommand: build_command,
+            language,
+            services: servicePaths.length,
+            main,
+            iconURL: icon
+                ? path.join("https://raw.githubusercontent.com/onesoft-sudo/sudobot/main/extensions/", extensionName, icon)
+                : null,
+            author:
+                typeof packageJson.author === "string"
+                    ? {
+                          name: packageJson.author
+                      }
+                    : packageJson.author,
+            license: packageJson.license,
+            licenseURL: `https://spdx.org/licenses/${packageJson.license}.html`,
+            homepage: packageJson.homepage,
+            repository: typeof packageJson.repository === "string" ? packageJson.repository : packageJson.repository?.url,
+            issues: typeof packageJson.bugs === "string" ? packageJson.bugs : packageJson.bugs?.url,
+            lastUpdated: new Date(),
+            readmeFileName: readmeFileName,
+            readmeFileURL: `https://raw.githubusercontent.com/onesoft-sudo/sudobot/main/extensions/${extensionName}/${readmeFileName}`
+        };
+    }
+
+    console.log("Overview of the extensions: ");
+    console.table(
+        Object.values(extensionsOutputRecord).map(e => ({
+            name: e.name,
+            commands: e.commands.length,
+            events: e.events.length
+        }))
+    );
+
+    const indexFile = path.join(extensionsPath, ".extbuilds/index.json");
+
+    writeFileSync(indexFile, JSON.stringify(extensionsOutputRecord, null, 4));
+
+    console.log("Wrote index file: ", indexFile);
+}
+
 if (process.argv.includes("--clear-cache") || process.argv.includes("--clean") || process.argv.includes("--delcache")) {
-    const indexFile = path.join(__dirname, "../extensions/index.json");
+    const indexFile = path.join(extensionsPath, "index.json");
 
     if (!existsSync(indexFile)) {
         error("No cached index file found!");
@@ -262,12 +384,20 @@ if (process.argv.includes("--clear-cache") || process.argv.includes("--clean") |
         console.log(e);
         process.exit(-1);
     });
+} else if (process.argv.includes("--mkindex")) {
+    console.log("Creating index for all the available extensions in the extensions/ directory");
+
+    writeExtensionIndex().catch(e => {
+        console.log(e);
+        process.exit(-1);
+    });
 } else {
     console.log("Usage:");
     console.log("  node extensions.js <options>\n");
     console.log("Options:");
-    console.log("  --build  |  Builds all the installed extensions, if needed.");
-    console.log("  --cache  |  Creates cache indexes for installed extensions, to improve the startup time.");
-    console.log("  --clean  |  Clears all installed extension cache.");
+    console.log("  --build    |  Builds all the installed extensions, if needed.");
+    console.log("  --cache    |  Creates cache indexes for installed extensions, to improve the startup time.");
+    console.log("  --clean    |  Clears all installed extension cache.");
+    console.log("  --mkindex  |  Creates indexes for all the available extensions, in the extensions/ top level directory.");
     process.exit(-1);
 }
