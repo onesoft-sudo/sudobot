38f136f7d24d7d77a93616ad834c8fcae3ff9ed0
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-02-08 15:53:15 +0600

    chore(extensions): build script now generates a list of download urls

--- scripts/extensions.js
+++ scripts/extensions.js
@@ -21,6 +21,7 @@
 
 require("module-alias/register");
 require("dotenv/config");
+
 const chalk = require("chalk");
 const { spawnSync } = require("child_process");
 const { existsSync, lstatSync, readdirSync, readFileSync, writeFileSync, rmSync } = require("fs");
@@ -28,6 +29,7 @@ const { readFile } = require("fs/promises");
 const path = require("path");
 const { chdir, cwd } = require("process");
 const { z } = require("zod");
+const semver = require('semver');
 
 const extensionsPath = process.env.EXTENSIONS_DIRECTORY ?? path.resolve(__dirname, "../extensions");
 
@@ -314,6 +316,29 @@ async function writeExtensionIndex() {
         const eventPaths = getRecuriveJavaScriptFiles(path.join(extensionDirectory, events));
         const servicePaths = getRecuriveJavaScriptFiles(path.join(extensionDirectory, services));
 
+        const tarballs = readdirSync(path.resolve(extensionsPath, ".extbuilds", extensionName));
+        
+        tarballs.sort((a, b) => {
+            const vA = path.basename(a).replace(`${extensionName}-`, '').replace(/\.tar\.gz$/ig, '');
+            const vB = path.basename(b).replace(`${extensionName}-`, '').replace(/\.tar\.gz$/ig, '');
+            const dashVA = vA.split('-')[1];
+            const dashVB = vB.split('-')[1];
+            const revA = isNaN(dashVA) ? 0 : parseInt(dashVA);
+            const revB = isNaN(dashVB) ? 0 : parseInt(dashVB);
+            const result = semver.rcompare(vA, vB);
+            return result === 0 ? revB - revA : result;
+        });
+        
+        const tarballList = tarballs.map(file => {
+            const basename = path.basename(file);
+            
+            return {
+                url: "https://raw.githubusercontent.com/onesoft-sudo/sudobot/main/extensions" + path.join('/.extbuilds/', extensionName, basename),
+                basename,
+                version: basename.replace(`${extensionName}-`, '').replace(/\.tar\.gz$/ig, '')
+            };
+        });
+
         extensionsOutputRecord[id] = {
             name,
             id,
@@ -340,7 +365,8 @@ async function writeExtensionIndex() {
             issues: typeof packageJson.bugs === "string" ? packageJson.bugs : packageJson.bugs?.url,
             lastUpdated: new Date(),
             readmeFileName: readmeFileName,
-            readmeFileURL: `https://raw.githubusercontent.com/onesoft-sudo/sudobot/main/extensions/${extensionName}/${readmeFileName}`
+            readmeFileURL: `https://raw.githubusercontent.com/onesoft-sudo/sudobot/main/extensions/${extensionName}/${readmeFileName}`,
+            tarballs: tarballList
         };
     }
 
