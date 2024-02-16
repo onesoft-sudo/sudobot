2f9d850fad007efb7cd3416d11dd0ae50f7f6043
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-02-16 18:19:19 +0600

    chore(extensions:urlfish): complete support for url scanning and extensive configuration

--- extensions/urlfish/src/events/message/NormalMessageCreateEventListener.ts
+++ extensions/urlfish/src/events/message/NormalMessageCreateEventListener.ts
@@ -1,17 +1,13 @@
 import EventListener from "@sudobot/core/EventListener";
 import { Events } from "@sudobot/types/ClientEvents";
-import type URLFishService from "../../services/URLFishService";
 import { Message } from "discord.js";
+import type URLFishService from "../../services/URLFishService";
 
 export default class NormalMessageCreateEventListener extends EventListener<Events.NormalMessageCreate> {
     public readonly name = Events.NormalMessageCreate;
 
     async execute(message: Message) {
         const urlfishService = this.client.getService<URLFishService>("urlfish");
-        const links = urlfishService.scanMessage(message);
-
-        if (links.length > 0) {
-            await message.delete();
-        }
+        urlfishService.verifyMessage(message);
     }
 }
