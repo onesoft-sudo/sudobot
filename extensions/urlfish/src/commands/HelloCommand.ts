b167ac582366b7d9c9304a1ca7163248c32365b6
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-02-17 10:02:39 +0000

    chore(extensions:urlfish): remove unneeded commands

--- extensions/urlfish/src/commands/HelloCommand.ts
+++
@@ -1,9 +0,0 @@
-import BaseCommand, { AnyCommandContext, CommandMessage, CommandReturn } from "@sudobot/core/Command";
-
-export default class HelloCommand extends BaseCommand {
-    public readonly name: string = "hello";
-
-    async execute(message: CommandMessage, context: AnyCommandContext): Promise<CommandReturn> {
-        message.reply("Hello world!");
-    }
-}
