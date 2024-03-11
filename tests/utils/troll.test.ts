44cd03f6c0e15935112568d8791494ccee59d98e
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-03-11 16:41:16 +0000

    test: add more tests for troll utils

--- tests/utils/troll.test.ts
+++ tests/utils/troll.test.ts
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker";
+import { Snowflake } from "discord.js";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import type Client from "../../src/core/Client";
 import ConfigManager, { GuildConfigContainer } from "../../src/services/ConfigManager";
@@ -10,11 +11,12 @@ import { randomSnowflake } from "../mocks/snowflakes";
 
 describe("troll functionalities", () => {
     let client: Client;
-    let sysAdminId, guildId;
+    let sysAdminId: Snowflake, guildId: Snowflake, normalUserId: Snowflake;
 
     beforeEach(() => {
         sysAdminId = randomSnowflake();
         guildId = randomSnowflake();
+        normalUserId = randomSnowflake();
         client = createClient();
         client.configManager = {
             systemConfig: SystemConfigSchema.parse({
@@ -36,14 +38,47 @@ describe("troll functionalities", () => {
         vi.resetAllMocks();
     });
 
-    it("should protect system admins from troll command", async () => {
-        const [message] = createMessage(faker.lorem.sentence(), sysAdminId, guildId);
+    it("should protect system admins and itself", async () => {
+        const [sysAdminMessage] = createMessage(faker.lorem.sentence(), sysAdminId, guildId);
+        const [selfMessage] = createMessage(faker.lorem.sentence(), client.user!.id, guildId);
         let count = 1;
 
         for (const key of ["bean_safe", "shot_safe", "fakeban_safe"] as const) {
-            const result = await protectSystemAdminsFromCommands(client, message, sysAdminId, key);
-            expect(result).toBe(true);
-            expect(message.reply).toHaveBeenCalledTimes(count++);
+            const sysAdminResult = await protectSystemAdminsFromCommands(
+                client,
+                sysAdminMessage,
+                sysAdminId,
+                key
+            );
+            const selfResult = await protectSystemAdminsFromCommands(
+                client,
+                selfMessage,
+                sysAdminId,
+                key
+            );
+
+            expect(sysAdminResult).toBe(true);
+            expect(selfResult).toBe(true);
+            expect(sysAdminMessage.reply).toHaveBeenCalledTimes(count);
+            expect(selfMessage.reply).toHaveBeenCalledTimes(count);
+
+            count++;
+        }
+    });
+
+    it("should ignore normal users", async () => {
+        const [message] = createMessage(faker.lorem.sentence(), normalUserId, guildId);
+
+        for (const key of ["bean_safe", "shot_safe", "fakeban_safe"] as const) {
+            const result = await protectSystemAdminsFromCommands(
+                client,
+                message,
+                normalUserId,
+                key
+            );
+
+            expect(result).toBe(false);
+            expect(message.reply).toHaveBeenCalledTimes(0);
         }
     });
 });
