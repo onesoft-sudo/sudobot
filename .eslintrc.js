c538039d009ed98ecd43fe5d16b07d28eea9c53a
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-03-20 00:52:29 +0600

    chore: update eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: true,
+        tsconfigRootDir: __dirname
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+        indent: "off",
+        "linebreak-style": ["error", "unix"],
+        quotes: [
+            "warn",
+            "double",
+            {
+                avoidEscape: true
+            }
+        ],
+        semi: ["warn", "always"],
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                caughtErrorsIgnorePattern: "^_"
+            }
+        ],
+        "@typescript-eslint/no-misused-promises": [
+            "warn",
+            {
+                checksVoidReturn: false
+            }
+        ]
+    },
+    overrides: [
+        {
+            files: ["*.d.ts"],
+            rules: {
+                "@typescript-eslint/no-unused-vars": "off"
+            }
+        }
+    ],
+    ignorePatterns: [
+        "*.js",
+        "./node_modules/**",
+        "./extensions/**",
+        "./docs/**",
+        "./*.bak/**",
+        "./tests/**"
+    ]
+};
