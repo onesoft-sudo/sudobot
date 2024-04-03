657b05f2165b7d243cbe2eb23d714eeb4b56d363
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-04-03 17:30:28 +0600

    build: blazebuild wrapper improvements

--- build_src/src/index.ts
+++ build_src/src/index.ts
@@ -1,6 +1,13 @@
 import BlazeBuild from "./core/BlazeBuild";
 
+declare global {
+    // eslint-disable-next-line no-var
+    var _cli: typeof cli;
+}
+
 const cli = BlazeBuild.getInstance();
+
+global._cli = cli;
 
 async function main() {
     await cli.setup();
