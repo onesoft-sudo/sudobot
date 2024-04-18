66a12c801841d725fc46a36166c0944ba9843755
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-04-18 16:22:12 +0000

    feat: add unban ccommand

---
+++ src/main/typescript/commands/moderation/UnbanCommand.ts
@@ -0,0 +1,118 @@
+/*
+ * This file is part of SudoBot.
+ *
+ * Copyright (C) 2021-2024 OSN Developers.
+ *
+ * SudoBot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SudoBot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { TakesArgument } from "@framework/arguments/ArgumentTypes";
+import { ErrorType } from "@framework/arguments/InvalidArgumentError";
+import RestStringArgument from "@framework/arguments/RestStringArgument";
+import UserArgument from "@framework/arguments/UserArgument";
+import { Buildable, Command, CommandMessage } from "@framework/commands/Command";
+import Context from "@framework/commands/Context";
+import { Inject } from "@framework/container/Inject";
+import { PermissionFlagsBits, User } from "discord.js";
+import { Limits } from "../../constants/Limits";
+import InfractionManager from "../../services/InfractionManager";
+import PermissionManagerService from "../../services/PermissionManagerService";
+
+type UnbanCommandArgs = {
+    user: User;
+    reason?: string;
+};
+
+@TakesArgument<UnbanCommandArgs>({
+    names: ["user"],
+    types: [UserArgument<true>],
+    optional: false,
+    rules: {
+        "interaction:no_required_check": true
+    },
+    errorMessages: [UserArgument.defaultErrors],
+    interactionName: "user"
+})
+@TakesArgument<UnbanCommandArgs>({
+    names: ["reason"],
+    types: [RestStringArgument],
+    optional: true,
+    errorMessages: [
+        {
+            [ErrorType.InvalidType]: "Invalid reason provided."
+        }
+    ],
+    interactionName: "reason",
+    interactionType: RestStringArgument
+})
+class UnbanCommand extends Command {
+    public override readonly name = "unban";
+    public override readonly description = "Unbans a user.";
+    public override readonly detailedDescription = "Removes the ban for the given user, if any.";
+    public override readonly permissions = [PermissionFlagsBits.BanMembers];
+    public override readonly defer = true;
+    public override readonly usage = ["<user: User> [reason: RestString]"];
+    public override readonly systemPermissions = [PermissionFlagsBits.BanMembers];
+
+    @Inject()
+    protected readonly infractionManager!: InfractionManager;
+
+    @Inject()
+    protected readonly permissionManager!: PermissionManagerService;
+
+    public override build(): Buildable[] {
+        return [
+            this.buildChatInput()
+                .addUserOption(option =>
+                    option.setName("user").setDescription("The user to unban.").setRequired(true)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName("reason")
+                        .setDescription("The reason for the unban.")
+                        .setMaxLength(Limits.Reason)
+                )
+        ];
+    }
+
+    public override async execute(
+        context: Context<CommandMessage>,
+        args: UnbanCommandArgs
+    ): Promise<void> {
+        const { user, reason } = args;
+
+        const result = await this.infractionManager.createUnban({
+            guildId: context.guildId,
+            moderator: context.user,
+            reason,
+            user,
+            generateOverviewEmbed: true
+        });
+        const { overviewEmbed, status } = result;
+
+        if (status === "failed") {
+            await context.error(
+                `Failed to unban the user. Maybe I don't have the permissions to do so. Error code: ${result.errorType ?? "unknown"}`
+            );
+
+            return;
+        }
+
+        await context.reply({
+            embeds: [overviewEmbed]
+        });
+    }
+}
+
+export default UnbanCommand;
