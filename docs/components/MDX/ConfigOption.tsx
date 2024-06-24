c542e74f907caa339ccab4277806df94bc64bde1
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-06-24 16:42:11 +0000

    docs: complete the automod rules page

--- docs/components/MDX/ConfigOption.tsx
+++ docs/components/MDX/ConfigOption.tsx
@@ -1,4 +1,6 @@
-import { ReactNode, type FC } from "react";
+import { Tooltip } from "@mui/material";
+import Link from "next/link";
+import { Fragment, ReactNode, type FC } from "react";
 
 type ConfigOptionProps = {
     as: FC | keyof JSX.IntrinsicElements;
@@ -9,14 +11,14 @@ type ConfigOptionProps = {
     defaultValue?: string;
 };
 
-const ConfigOption: FC<ConfigOptionProps> = ({
+export function ConfigOption({
     as: Element = "div",
     type,
     optionKey,
     children,
     optional,
     defaultValue,
-}) => {
+}: ConfigOptionProps) {
     return (
         <Element className="bg-gray-50 dark:bg-[rgba(255,255,255,0.08)] p-2 rounded-lg block my-4">
             <div className="font-bold md:text-lg mb-3 flex justify-between items-center not-prose">
@@ -70,6 +72,66 @@ const ConfigOption: FC<ConfigOptionProps> = ({
             </div>
         </Element>
     );
-};
+}
 
-export default ConfigOption;
+export namespace ConfigOption {
+    const Union = ({ children }: { children: ReactNode[] }) => (
+        <>
+            {children.map((type, index) => (
+                <Fragment key={index}>
+                    {index > 0 && <span className="mx-2">|</span>}
+                    {type}
+                </Fragment>
+            ))}
+        </>
+    );
+
+    const StringLiteral = (
+        props: { value: ReactNode } | { children: ReactNode },
+    ) => (
+        <span className="text-yellow-600">
+            "{"value" in props ? props.value : props.children}"
+        </span>
+    );
+
+    const ArrayLiteral = ({ children }: { children: ReactNode }) => (
+        <>
+            <span>
+                {Array.isArray(children) ? <>({children})</> : children}
+            </span>
+            <span className="text-blue-500">[]</span>
+        </>
+    );
+
+    const Identifier = ({
+        children,
+        url,
+    }: {
+        children: ReactNode;
+        url?: string;
+    }) => (
+        <span className="text-teal-400">
+            {url ? (
+                <Tooltip title="Click to go the definition of this type">
+                    <Link href={url} target="_blank" rel="noreferrer">
+                        {children}
+                    </Link>
+                </Tooltip>
+            ) : (
+                children
+            )}
+        </span>
+    );
+
+    function Null() {
+        return <Identifier>null</Identifier>;
+    }
+
+    export const Types = {
+        Union,
+        StringLiteral,
+        ArrayLiteral,
+        Identifier,
+        Null,
+    };
+}
