5ce34df964e5957b3f319baebd0f26b2578fda09
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-07-12 20:47:46 +0600

    chore: debug
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- blazebuild/src/main/typescript/wrapper/SDKManager.ts
+++ blazebuild/src/main/typescript/wrapper/SDKManager.ts
@@ -14,6 +14,7 @@ class SDKManager extends UsesWrapper {
     private findInPath(executable: string) {
         const PATH = process.env.PATH ?? "";
         const paths = PATH.split(process.platform === "win32" ? ";" : ":");
+        console.log(paths);
 
         for (const p of paths) {
             const fullPath = path.join(p, executable);
