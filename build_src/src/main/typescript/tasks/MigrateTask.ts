b11dfca178a1ece5c8ebe84bb2587c62e8f96f87
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-07-29 01:00:08 +0600

    build: update migrate task
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- build_src/src/main/typescript/tasks/MigrateTask.ts
+++ build_src/src/main/typescript/tasks/MigrateTask.ts
@@ -7,16 +7,15 @@ import { AbstractTask, IO, Task, TaskAction } from "blazebuild";
 class MigrateTask extends AbstractTask {
     @TaskAction
     protected override async run(): Promise<void> {
-        IO.newline();
-
         if (!process.env.DB_URL) {
+            IO.newline();
             IO.fatal("DB_URL environment variable is not set");
         }
 
         const { drizzle } = await import(String("drizzle-orm/node-postgres"));
         const { migrate } = await import(String("drizzle-orm/node-postgres/migrator"));
         const { Pool } = await import(String("pg"));
-        
+
         const pool = new Pool({
             connectionString: process.env.DB_URL
         });
