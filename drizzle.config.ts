fda23c969ac636107b01c1f71eca22d7815f3e56
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-07-29 00:53:34 +0600

    build: update drizzle.config.ts
    Signed-off-by: Ar Rakin <68149013+virtual-designer@users.noreply.github.com>

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,25 @@
-import "dotenv/config";
-import { defineConfig } from "drizzle-kit";
-import { readdirSync } from "fs";
-import path from "path";
+require("dotenv/config");
+const { readdirSync } = require("fs");
+const path = require("path");
 
-export default defineConfig({
-    dialect: "postgresql",
-    out: "./drizzle",
-    schema: readdirSync(path.resolve(__dirname, "src/main/typescript/models")).map(file =>
-        path.resolve(__dirname, "src/main/typescript/models", file)
-    ),
-    dbCredentials: {
-        url: process.env.DB_URL
-    },
-    // Print all statements
-    verbose: true,
-    // Always ask for confirmation
-    strict: true
-});
+try {
+    const { defineConfig } = require("drizzle-kit");
+    
+    module.exports = defineConfig({
+        dialect: "postgresql",
+        out: "./drizzle",
+        schema: readdirSync(path.resolve(__dirname, "src/main/typescript/models")).map(file =>
+            path.resolve(__dirname, "src/main/typescript/models", file)
+        ),
+        dbCredentials: {
+            url: process.env.DB_URL
+        },
+        // Print all statements
+        verbose: true,
+        // Always ask for confirmation
+        strict: true
+    });
+}
+catch (error) {
+    console.error(error);
+}
