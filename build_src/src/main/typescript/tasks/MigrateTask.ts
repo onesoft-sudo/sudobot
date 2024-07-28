35b9b91f518171d7847620bfead11fa82afb3b0c
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-07-29 00:39:34 +0600

    build: update blazebuild tasks to not use drizzle-kit
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- build_src/src/main/typescript/tasks/MigrateTask.ts
+++ build_src/src/main/typescript/tasks/MigrateTask.ts
@@ -1,20 +1,28 @@
-import { AbstractTask, IO, Task, TaskAction, TaskDependencyGenerator } from "blazebuild";
-import { $ } from "bun";
+import { AbstractTask, IO, Task, TaskAction } from "blazebuild";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { Pool } from "pg";
 
 @Task({
     description: "Runs the database migrations",
     group: "Database"
 })
 class MigrateTask extends AbstractTask {
-    @TaskDependencyGenerator
-    protected override dependencies() {
-        return ["generateMigrations"];
-    }
-
     @TaskAction
     protected override async run(): Promise<void> {
         IO.newline();
-        await $`npx drizzle-kit migrate`;
+
+        if (!process.env.DB_URL) {
+            IO.fatal("DB_URL environment variable is not set");
+        }
+
+        const pool = new Pool({
+            connectionString: process.env.DB_URL
+        });
+
+        const db = drizzle(pool);
+        await migrate(db, { migrationsFolder: "drizzle" });
+        await pool.end();
     }
 }
 
