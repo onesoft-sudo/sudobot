8aa3c261fc06bc845dad6d597cebe187ada65849
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-08-18 08:52:19 +0600

    docs: update broken links
    Signed-off-by: Ar Rakin <68149013+virtual-designer@users.noreply.github.com>

--- docs/utils/links.ts
+++ docs/utils/links.ts
@@ -1,4 +1,4 @@
-export const DONATION_URL = "https://www.sudobot.org/donate";
+export const DONATION_URL = "https://sudobot.onesoftnet.eu.org/donate";
 export const BOT_INVITE_REQUEST_URL = "https://forms.gle/943kW9q25MpKEwW26";
 export const SUPPORT_EMAIL_ADDRESS = "rakinar2@onesoftnet.eu.org";
 export const GITHUB_REPO_URL = "https://github.com/onesoft-sudo/sudobot";
