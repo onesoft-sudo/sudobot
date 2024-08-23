3bcbca036a0fb274a6c053e50730239dd4f3eda8
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2024-08-23 14:27:07 +0600

    fix(blazebuild): column number resolves to Infinity when on non-TTY environment
    Signed-off-by: Ar Rakin <68149013+virtual-designer@users.noreply.github.com>

--- blazebuild/src/main/typescript/io/IO.ts
+++ blazebuild/src/main/typescript/io/IO.ts
@@ -13,6 +13,14 @@ class IO {
         this._quiet = quiet;
     }
 
+    private static noInfinity(value: number) {
+        if (Number.isFinite(value)) {
+            return value;
+        }
+
+        return 0;
+    }
+
     public static createProgress(total: number): void {
         if (!process.stdout.isTTY || this._quiet) {
             return;
@@ -41,7 +49,7 @@ class IO {
 
         console[consoleMethod].call(
             console,
-            typeof message === "string" ? message.padEnd(process.stdout.columns, " ") : message
+            typeof message === "string" ? message.padEnd(this.noInfinity(process.stdout.columns), " ") : message
         );
     }
 
@@ -51,7 +59,7 @@ class IO {
         }
 
         if (this._progress) {
-            console.log(`\r${" ".repeat(process.stdout.columns - 1)}`);
+            console.log(`\r${" ".repeat(this.noInfinity(process.stdout.columns - 1))}`);
             return;
         }
 
@@ -160,10 +168,10 @@ class IO {
 
         this.destroyProgress();
 
-        console.log("\r" + " ".repeat(process.stdout.columns - 1));
+        console.log("\r" + " ".repeat(this.noInfinity(process.stdout.columns - 1));
         console.log(
             `${chalk.green.bold("BUILD SUCCESSFUL")} in ${this.timeDiffFromStartup()}`.padEnd(
-                process.stdout.columns,
+                this.noInfinity(process.stdout.columns),
                 " "
             )
         );
@@ -182,7 +190,7 @@ class IO {
         }
 
         this.destroyProgress();
-        console.log("\r" + " ".repeat(process.stdout.columns - 1));
+        console.log("\r" + " ".repeat(this.noInfinity(process.stdout.columns) - 1));
         console.log(`${chalk.red.bold("BUILD FAILED")} in ${this.timeDiffFromStartup()}`);
     }
 
