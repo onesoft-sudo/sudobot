94fbed1d66e7ad0e17e8bd10f767e952dd2b01f5
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-08-25 12:38:07 +0000

    refactor(extensions:neko): fully support sudobot v9 APIs
    Signed-off-by: GitHub <noreply@github.com>

--- extensions/neko/src/index.ts
+++ extensions/neko/src/index.ts
@@ -1,4 +1,14 @@
 import { Extension } from "@sudobot/extensions/Extension";
+import { Awaitable } from "discord.js";
 import "module-alias/register";
+import Application from "../../../build/out/framework/typescript/app/Application";
+import { Command } from "../../../build/out/framework/typescript/commands/Command";
+import { Class } from "../../../build/out/framework/typescript/types/Utils";
+import AnimeCommand from "./commands/AnimeCommand";
+import NekoCommand from "./commands/NekoCommand";
 
-export default class Neko extends Extension {}
+export default class Neko extends Extension {
+    protected commands(): Awaitable<Class<Command, [Application]>[]> {
+        return [AnimeCommand, NekoCommand];
+    }
+}
