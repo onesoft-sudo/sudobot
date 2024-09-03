8108ffde6f61d86870ada53ad88a49e7938161d8
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-09-03 18:46:54 +0000

    feat(utils:axios): log outgoing http requests
    Signed-off-by: GitHub <noreply@github.com>

--- src/main/typescript/utils/axios.ts
+++ src/main/typescript/utils/axios.ts
@@ -1,20 +1,29 @@
 import type Application from "@framework/app/Application";
+import { Logger } from "@framework/log/Logger";
 import type { AxiosInstance } from "axios";
 import axios from "axios";
 
+const logger = new Logger("http", true);
 let _axiosClient: AxiosInstance;
 
 export const createAxiosClient = (application: Application) => {
     const configUserAgent =
         process.env.HTTP_USER_AGENT === null
             ? undefined
-            : process.env.HTTP_USER_AGENT ?? `SudoBot/${application.version}`;
+            : (process.env.HTTP_USER_AGENT ?? `SudoBot/${application.version}`);
 
     _axiosClient = axios.create({
         headers: {
             "Accept-Encoding": process.isBun ? "gzip" : undefined,
             "User-Agent": configUserAgent
         }
+    });
+
+    _axiosClient.interceptors.request.use(config => {
+        logger.info(
+            `${config.method?.toUpperCase()} ${config.url} - ${config.headers["User-Agent"] ?? "No User-Agent"}`
+        );
+        return config;
     });
 
     return _axiosClient;
