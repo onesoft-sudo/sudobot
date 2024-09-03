619a10ea196bfe624028ee4065d680fbae1bf029
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-09-03 17:42:47 +0000

    feat: updated api announcement
    Signed-off-by: GitHub <noreply@github.com>

--- src/main/typescript/api/controllers/AnnouncementController.ts
+++ src/main/typescript/api/controllers/AnnouncementController.ts
@@ -7,9 +7,9 @@ class AnnouncementController extends Controller {
         return {
             title: "Announcement",
             content:
-                "We're here to announce that SudoBot 9.x \"Chocolate Cake\" is now available! 🎉🍰🎉\n\nThis release adds a bunch of new features, and performance fixes. We're open to any questions or suggestions as always, feel free to contact us anytime! We hope you have a great day.",
+                "We're here to announce that SudoBot 10.x \"Delicious Donut\" is now available! 🎉🍰🎉\n\nThis release adds a bunch of new features, and performance fixes. We're open to any questions or suggestions as always, feel free to contact us anytime! We hope you have a great day.",
             from: "rakinar2",
-            timestamp: 1715616799882
+            timestamp: 1725385295628
         };
     }
 }
