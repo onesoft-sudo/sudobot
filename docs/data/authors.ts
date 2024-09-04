25f9d779a38a29f9d39fc94fd9af23ecc23acc84
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-09-04 17:48:11 +0000

    docs: add v10 release blog post
    Signed-off-by: GitHub <noreply@github.com>

--- docs/data/authors.ts
+++ docs/data/authors.ts
@@ -1,7 +1,7 @@
 export const Authors = {
     AR_RAKIN: {
         name: "Ar Rakin",
-        link: "mailto:rakinar2@onesoftnet.eu.org",
+        link: "mailto:rakinar2@sudobot.online",
         icon: "https://avatars.githubusercontent.com/virtual-designer",
     },
 };
