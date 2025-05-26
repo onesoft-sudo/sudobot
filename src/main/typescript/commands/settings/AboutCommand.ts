b8ce7c0bc82e7ae0fb3a024aad21482298613f50
Ar Rakin <rakinar2@onesoftnet.eu.org>
2025-05-26 21:47:52 +0600

    feat: start using discord components v2
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- src/main/typescript/commands/settings/AboutCommand.ts
+++ src/main/typescript/commands/settings/AboutCommand.ts
@@ -1,7 +1,7 @@
 /*
  * This file is part of SudoBot.
  *
- * Copyright (C) 2021, 2022, 2023, 2024 OSN Developers.
+ * Copyright (C) 2021, 2022, 2023, 2024, 2025 OSN Developers.
  *
  * SudoBot is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by
@@ -20,7 +20,17 @@
 import type { ChatContext } from "@framework/commands/Command";
 import { Command } from "@framework/commands/Command";
 import { getEnvData } from "@main/env/env";
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ContainerBuilder,
+    MessageFlags,
+    SectionBuilder,
+    SeparatorSpacingSize,
+    TextDisplayBuilder,
+    ThumbnailBuilder
+} from "discord.js";
 import type { MetadataType } from "../../core/DiscordKernel";
 
 class AboutCommand extends Command {
@@ -36,78 +46,108 @@ class AboutCommand extends Command {
         const codeName = metadata._meta.release_codename;
         const shortCodeName = metadata._meta.release_short_codename;
 
-        await context.reply({
-            embeds: [
-                {
-                    thumbnail: avatar
-                        ? {
-                              url: avatar
-                          }
-                        : undefined,
-                    description: (
-                        `
-                        ## ${emoji ? emoji.toString() + " " : ""}SudoBot ${shortCodeName}\n
-                        ### A free and open source Discord moderation bot.\n
-                        This bot is **free software**, and you are welcome to redistribute it under certain conditions.
-                        If you make changes to the bot, you must make the source code of the modified version available to the public, under the same license.
-                        See the [GNU Affero General Public License v3](https://www.gnu.org/licenses/agpl-3.0.en.html) for more detailed information.
-                    ` +
-                        (env.HIDE_MODIFICATIONS_URL_NOTICE !== "1" && !env.MODIFICATIONS_PUBLIC_URL
-                            ? `
-                        **Notice:** If you make changes to the bot, as stated above, please make your changes public, and set the \`MODIFICATIONS_PUBLIC_URL\` environment variable to the URL of the source code of the modified version, which should be publicly accessible.
-                        To hide this notice, set the \`HIDE_MODIFICATIONS_URL_NOTICE\` environment variable to \`"1"\`.    
-                        `
-                            : "") +
-                        (env.MODIFICATIONS_PUBLIC_URL
-                            ? `
-                        ### Modifications\n
-                        This bot has been modified by the developers of this instance.\n
-                        According to **The GNU Affero General Public License v3**, the source code of the modifications must be made available to the public, under the same license.\n
-                        You can view the source code of this modified version [here](${env.MODIFICATIONS_PUBLIC_URL}).
-                        `
-                            : "")
-                    ).replaceAll(/\n([ \t]+)/gm, "\n"),
-                    color: 0x007bff,
-                    fields: [
-                        {
-                            name: "Version",
-                            value: `**${metadata.version}** (${codeName})`,
-                            inline: true
-                        },
-                        {
-                            name: "Source Code",
-                            value: `[GitHub](${metadata.repository.url})`,
-                            inline: true
-                        },
-                        {
-                            name: "Author",
-                            value: `[${metadata.author.name}](${metadata.author.url})`,
-                            inline: true
-                        },
-                        {
-                            name: "Support",
-                            value: "[Contact Us](https://docs.sudobot.onesoftnet.eu.org/getting-started#help--support)",
-                            inline: true
-                        },
-                        {
-                            name: "Documentation",
-                            value: "[SudoBot Docs](https://docs.sudobot.onesoftnet.eu.org)",
-                            inline: true
-                        }
-                    ],
-                    footer: {
-                        text: `Copyright © OSN Developers and the contributors 2022-${new Date().getFullYear()}.`
-                    }
-                }
-            ],
-            components: [
-                new ActionRowBuilder<ButtonBuilder>().addComponents(
-                    new ButtonBuilder()
-                        .setStyle(ButtonStyle.Link)
-                        .setURL(metadata.funding.url)
-                        .setLabel("Donate")
-                )
+        const container = new ContainerBuilder();
+        const topHeader = new TextDisplayBuilder().setContent(
+            [
+                `## ${emoji ? emoji.toString() + " " : ""}SudoBot ${shortCodeName}`,
+                "### A free and open source Discord moderation bot.",
+                "This bot is **free software**, and you are welcome to redistribute it under certain conditions.",
+                "If you make changes to the bot, you must make the source code of the modified version available to the public, under the same license.",
+                "See the [GNU Affero General Public License v3](https://www.gnu.org/licenses/agpl-3.0.en.html) for more detailed information.",
+                env.HIDE_MODIFICATIONS_URL_NOTICE !== "1" &&
+                !env.MODIFICATIONS_PUBLIC_URL
+                    ? "\n**Notice:** If you make changes to the bot, as stated above, please make your changes public, and set the `MODIFICATIONS_PUBLIC_URL` environment variable to the URL of the source code of the modified version, which should be publicly accessible.\n" +
+                      'To hide this notice, set the `HIDE_MODIFICATIONS_URL_NOTICE` environment variable to `"1"`'
+                    : "",
+                env.MODIFICATIONS_PUBLIC_URL
+                    ? "\n### Modifications" +
+                      "This bot has been modified by the developers of this instance.\n" +
+                      "According to **The GNU Affero General Public License v3**, the source code of the modifications must be made available to the public, under the same license.\n" +
+                      "You can view the source code of this modified version [here](${env.MODIFICATIONS_PUBLIC_URL})."
+                    : ""
             ]
+                .filter(Boolean)
+                .join("\n")
+        );
+
+        if (avatar) {
+            const topSection = new SectionBuilder().addTextDisplayComponents(
+                topHeader
+            );
+
+            topSection.setThumbnailAccessory(
+                new ThumbnailBuilder().setURL(avatar)
+            );
+
+            container.addSectionComponents(topSection);
+        } else {
+            container.addTextDisplayComponents(topHeader);
+        }
+
+        container.addSeparatorComponents(separator =>
+            separator.setSpacing(SeparatorSpacingSize.Large)
+        );
+
+        const versionSection = new SectionBuilder()
+            .addTextDisplayComponents(
+                new TextDisplayBuilder().setContent(
+                    `**Version:** ${metadata.version}\n**Codename:** ${codeName}`
+                )
+            )
+            .setButtonAccessory(
+                new ButtonBuilder()
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(metadata.repository.url)
+                    .setLabel("Source Code")
+            );
+
+        container.addSectionComponents(versionSection);
+        container.addTextDisplayComponents(
+            new TextDisplayBuilder().setContent(
+                `**Author:** [${metadata.author.name}](${metadata.author.url})`
+            )
+        );
+
+        const supportSection = new SectionBuilder()
+            .addTextDisplayComponents(
+                new TextDisplayBuilder().setContent("### Need Help?")
+            )
+            .setButtonAccessory(
+                new ButtonBuilder()
+                    .setStyle(ButtonStyle.Link)
+                    .setURL("https://docs.sudobot.onesoftnet.eu.org")
+                    .setLabel("Documentation")
+            );
+
+        container.addSectionComponents(supportSection);
+        container.addActionRowComponents(
+            new ActionRowBuilder<ButtonBuilder>().addComponents(
+                new ButtonBuilder()
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(
+                        "https://docs.sudobot.onesoftnet.eu.org/getting-started#help--support"
+                    )
+                    .setLabel("Contact Us"),
+                new ButtonBuilder()
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(metadata.funding.url)
+                    .setLabel("Donate")
+            )
+        );
+
+        container.addSeparatorComponents(separator =>
+            separator.setSpacing(SeparatorSpacingSize.Large)
+        );
+
+        container.addTextDisplayComponents(
+            new TextDisplayBuilder().setContent(
+                `-# Copyright © OSN Developers and the contributors 2022-${new Date().getFullYear()}.`
+            )
+        );
+
+        await context.reply({
+            flags: MessageFlags.IsComponentsV2,
+            components: [container]
         });
     }
 }
