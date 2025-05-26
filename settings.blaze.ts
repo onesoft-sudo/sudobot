280da1c6f1d7c15a3b66d971b24922fb71456829
Ar Rakin <rakinar2@onesoftnet.eu.org>
2025-05-26 22:30:16 +0600

    chore: prepare for v11
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- settings.blaze.ts
+++ settings.blaze.ts
@@ -1,7 +1,7 @@
 import { project, settings } from "@onesoftnet/blazebuild";
 
 project.name = "@onesoftnet/sudobot";
-project.version = "10.54.2";
+project.version = "11.0.0";
 project.description = "A Discord bot for moderation purposes.";
 project.author = { name: "Ar Rakin", email: "rakinar2@onesoftnet.eu.org" };
 project.license = "AGPL-3.0-or-later";
