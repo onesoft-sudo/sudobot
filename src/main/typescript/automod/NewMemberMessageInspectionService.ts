2b9a2756af21e88fa97a943ecf420c7ca14d552a
Ar Rakin <rakinar2@osndevs.org>
2025-09-16 16:10:11 +0600

    fix: type errors
    Signed-off-by: Ar Rakin <rakinar2@osndevs.org>

--- src/main/typescript/automod/NewMemberMessageInspectionService.ts
+++ src/main/typescript/automod/NewMemberMessageInspectionService.ts
@@ -284,7 +284,7 @@ class NewMemberMessageInspectionService extends Service implements HasEventListe
               )
             : null;
 
-        await Promise.all([promise1, promise2] as unknown as Promise[]);
+        await Promise.all([promise1, promise2] as unknown as Promise<unknown>[]);
     }
 
     private async paxmodModerateText(text: string) {
