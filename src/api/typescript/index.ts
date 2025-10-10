7e9aa1b38df83f97b3499384a12d0cfb9f01c30a
Ar Rakin <rakinar2@osndevs.org>
2025-10-10 22:14:25 +0600

    fix(sudobot-api): publishing issues
    Signed-off-by: Ar Rakin <rakinar2@osndevs.org>

--- src/api/typescript/index.ts
+++ src/api/typescript/index.ts
@@ -1,5 +1,8 @@
 export * from "./APIEndpoints";
 export * from "./APIErrorCode";
+export * from "./models/Guild";
+export * from "./models/User";
 export * from "./responses/AuthResponse";
+export * from "./utils/Snowflake";
 
 export const API_VERSION = 1;
