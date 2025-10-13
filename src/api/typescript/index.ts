8d47edc099545b6bbc6c6ec1fb3aa65c0267852f
Ar Rakin <rakinar2@osndevs.org>
2025-10-13 18:31:28 +0600

    fix(sudobot-api): add correct exports
    Signed-off-by: Ar Rakin <rakinar2@osndevs.org>

--- src/api/typescript/index.ts
+++ src/api/typescript/index.ts
@@ -3,6 +3,8 @@ export * from "./APIErrorCode";
 export * from "./models/Guild";
 export * from "./models/User";
 export * from "./responses/AuthResponse";
+export * from "./responses/GetGuildConfigurationResponse";
+export * from "./responses/SetGuildConfigurationResponse";
 export * from "./utils/Snowflake";
 
 export const API_VERSION = 1;
